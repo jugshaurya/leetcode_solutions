bbfb84b1ff1ff3297f49852f479db9f3d4cefc1b
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-01-31 15:02:36 +0530

    Time: 774 ms (14.28%), Space: 174.2 MB (27.91%) - LeetHub

---
+++ 146-lru-cache/146-lru-cache.cpp
@@ -0,0 +1,62 @@
+class LRUCache {
+public:
+    int capacity;
+    int cnt;
+    list<int> l;
+    unordered_map<int, pair<int, list<int>::iterator>> m;
+    
+    LRUCache(int capacity) {
+        this->capacity = capacity;
+        cnt=0;
+    }
+    
+    int get(int key) {
+        if(m.count(key)){
+            list<int>::iterator index = m[key].second;
+            l.erase(index);
+            l.push_back(key);
+            auto it = l.end();
+            it--;
+            m[key] = {m[key].first, it};
+            return m[key].first;
+        }else{
+            return -1;
+        }
+    }
+    
+    void put(int key, int value) {
+        if(m.count(key)){
+            list<int>::iterator index = m[key].second;
+            l.erase(index);
+            l.push_back(key);
+            auto it = l.end();
+            it--;
+            m[key] = {value, it};
+        }else{
+            if(cnt==capacity){
+                // replace LRU node with new node
+                int frontVal = l.front();
+                l.pop_front();
+                m.erase(frontVal);
+                l.push_back(key);
+                auto it = l.end();
+                it--;
+                m[key] = {value, it};
+                return;
+            }
+            cnt++;
+            l.push_back(key);
+            auto it = l.end();
+            it--;
+            m[key] = {value, it};
+            return;
+        }
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
