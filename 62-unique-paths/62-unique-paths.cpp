271ca4b8e38a7a868de580e3d1dc3766472379b4
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-02-06 22:19:34 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (98.48%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -10,11 +10,6 @@ public:
             for(int j=1;j<n;j++)
                 dp[i][j] = dp[i-1][j] + dp[i][j-1]  ;              
     
-        for(int i=1;i<m;i++)        
-            for(int j=1;j<n;j++)
-                cout<<dp[i][j]<<" ";
-            
-    
         return dp[m-1][n-1];
     }
 };
