d05a63dbbd5fa21955aa4c582e7bd7775004acc5
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-02-13 12:03:28 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (85.71%) - LeetHub

--- 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
@@ -1,12 +1,16 @@
 
 class Solution {
   public:
-  int count(int a, int b) {
-    if (b == 0)
-      return 0;
-    if (a > b)
-      return 1 + count(b, a - b);
-    return 1 + count(a, b - a);
+  int count(int l, int s) {
+        int count = 0;
+      while(l!=0 and s!=0){
+          int nl = max(s, l-s);
+          int ns = min(s,l-s);
+          count++;
+          l = nl;
+          s = ns;
+      }
+    return count;      
   }
 
   int countOperations(int num1, int num2) {
