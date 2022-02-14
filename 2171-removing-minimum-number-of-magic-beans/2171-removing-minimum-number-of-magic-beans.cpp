186c1d94dbfd4fa80c923e502d8c5d3df0e96cd5
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-02-14 13:06:13 +0530

    Time: 389 ms (41.18%), Space: 100.7 MB (70.59%) - LeetHub

--- 2171-removing-minimum-number-of-magic-beans/2171-removing-minimum-number-of-magic-beans.cpp
+++ 2171-removing-minimum-number-of-magic-beans/2171-removing-minimum-number-of-magic-beans.cpp
@@ -1,31 +1,19 @@
 class Solution {
 
 public:
-    void print(vector<int>& beans){
-        for(auto x: beans)cout<<x<<" ";
-        cout<<endl;
-    }
-    
     long long minimumRemoval(vector<int>& beans) {
-        
-        sort(beans.begin(), beans.end());
-        int n = beans.size(); 
-        if(n==1) return 0;
 
-        vector<long long> prefixSum(n+1);
-        prefixSum[0] = 0;
-        for(int i = 0; i<n; i++){
-            prefixSum[i+1] = beans[i] + prefixSum[i];
-        }
+        int n = beans.size(); 
+        if(n == 1) return 0;
+
+        sort(beans.begin(), beans.end());
+        long long sum = accumulate(beans.begin(), beans.end(), 0L);
         
-        // case if i don't make anything zero
-        // answer would be minimum-beans[i], for all 0<=i<n
-        long long ans = (prefixSum[n] - prefixSum[0]) - (long long)beans[0]*(n); 
-        for(int i = 0; i<n-1;i++){
-            long long temp = prefixSum[i+1];
-            temp += (prefixSum[n] - prefixSum[i+1]) - (long long)beans[i+1]*(n-i-1);
-            ans = min(ans, temp);
+        long long ans = 1e18;
+        for(int i = 0; i < n; i++){
+            ans = min(ans, sum - (long long)beans[i]*(n-i));
         }        
+        
         return ans;
     }
 };
