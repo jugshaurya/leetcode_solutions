cd0301be82cf2023b3c6aefaff4a5d888fd4f6b6
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-04-11 11:38:38 +0530

    Time: 1063 ms (6.76%), Space: 146.3 MB (17.42%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,26 +1,60 @@
 class Solution {
 public:
-    vector<string> arr;
-    int helper(int i, string ans){
+    
+    // Method 1: simple recursion O(2^n * 26)    
+    // vector<string> arr;
+    //     int helper(int i, string ans){
+    //         if(i==arr.size()) {
+    //             if(ans.size() > 26) return INT_MIN;
+    //             sort(ans.begin(), ans.end());
+    //             for(int i = 1; i< ans.size(); i++){
+    //                 if(ans[i]==ans[i-1]) return INT_MIN;
+    //             }
+    //             return ans.size();
+    //         }
 
-        if(i==arr.size()) {
-            if(ans.size() > 26) return INT_MIN;
-            sort(ans.begin(), ans.end());
-            for(int i = 1; i< ans.size(); i++){
-                if(ans[i]==ans[i-1]) return INT_MIN;
+    //         int opt1 = helper(i+1, ans);
+    //         int opt2 = INT_MIN;
+    //         ans += arr[i]; 
+    //         if(ans.size() <= 26) opt2 = helper(i+1, ans);
+    //         return max(opt1, opt2);
+    //     }
+
+    //     int maxLength(vector<string>& temp) {
+    //         arr = temp;
+    //         return helper(0, "");
+    //     }
+    
+    //  Method 2: Dp + Bitmasking O(2^n * 26)
+    
+    vector<string> arr;
+    
+    int helper(int i, int mask){
+        int n = arr.size();
+        if(i == n) {
+            int j = n - 1;
+            string temp;
+            while (mask) {
+                if(mask&1)  temp += arr[j];
+                if(temp.size() > 26) return INT_MIN;
+                mask >>= 1;
+                j--;
             }
-            return ans.size();
-        }
+ 
+            sort(temp.begin(), temp.end());
+            for(int i = 1; i < temp.size(); i++){
+                if(temp[i] == temp[i-1]) return INT_MIN;
+            }
             
-        int opt1 = helper(i+1, ans);
-        int opt2 = INT_MIN;
-        ans += arr[i]; 
-        if(ans.size() <= 26) opt2 = helper(i+1, ans);
-        return max(opt1, opt2);
+            return temp.size();
+        }
+        
+        return max(helper(i+1, mask), helper(i+1, (mask| (1<<i))));
     }
     
     int maxLength(vector<string>& temp) {
         arr = temp;
-        return helper(0, "");
+        return helper(0, 0);
     }
+    
 };
