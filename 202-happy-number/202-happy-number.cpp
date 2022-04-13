6a1cafa3440600150b91cc4c8e1a9946b3966081
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-04-13 19:20:13 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (83.94%) - LeetHub

---
+++ 202-happy-number/202-happy-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    
+    int getSumOfSquares(int n){
+      int sum = 0;
+      while(n){
+        sum += (n%10)*(n%10); 
+        n/=10;
+      }
+      
+      return sum;
+    }
+  
+    bool isHappy(int n) {
+      int t = n;
+      int cnt = 0; 
+      while(cnt<1000){
+        cnt++;
+        t = getSumOfSquares(t);
+        if(t==1) return true;
+      }
+      
+      return false;
+    }
+};
