e66bbf8a4f571d7e17a9c41c7bdbfc5da8c101bf
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-04-15 12:48:12 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (72.85%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,25 +1,26 @@
 class Solution {
-public:
-    int dp[5005];
-    string s;
+  public:
+  
+  int dp[5005];
+  string s;
+  int ways(int i = 0) {
+    if (i == s.size()) return 1;
+    if (s[i] == '0') return 0;
+    if (i + 1 == s.size() and s[i] == '0') return 0;
+    if (i + 1 == s.size() and s[i] != '0') return 1;
 
-    int ways(int i = 0) {
-      if(i==s.size()) return 1;
-      if(i+1==s.size() and s[i]=='0') return 0;
-      if(i+1==s.size() and s[i]!='0') return 1;
-      if(s[i]=='0') return 0;
-      if(dp[i]!=-1) return dp[i];
-      int ans = 0;
-      if(s[i]>='1' and s[i]<='9') ans += ways(i + 1);
-      if (s[i]=='1') ans += ways(i + 2);
-      if (i + 1 < s.size() and (s[i]=='2' and s[i+1]<='6')) ans += ways(i + 2);
-      return dp[i] = ans;
-    }
+    if (dp[i] != -1) return dp[i];
 
-    int numDecodings(string str) {
-      s = str;
-      memset(dp, -1, sizeof(dp));
-      if(s[0]=='0') return 0;
-      return ways();
-    }
+    int ans = 0;
+    if (s[i] >= '1' and s[i] <= '9') ans += ways(i + 1);
+    if (s[i] == '1') ans += ways(i + 2);
+    if (i + 1 < s.size() and (s[i] == '2' and s[i + 1] <= '6')) ans += ways(i + 2);
+    return dp[i] = ans;
+  }
+
+  int numDecodings(string str) {
+    s = str;
+    memset(dp, -1, sizeof(dp));
+    return ways();
+  }
 };
