60a41aef1551e6bde86b0a9fe307f329d2937bef
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-04-17 13:13:53 +0530

    Time: 31 ms (56.42%), Space: 19.1 MB (98.36%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -5,15 +5,32 @@ public:
       int i = 0;
       int j = 0;
       
+      //  without using else statement
+//       while(i<n and j<n) {
+//         if(nums[i] != 0) {
+//           i++;
+//           j++;
+//         }
+        
+//         if(i<n and j<n and nums[i] == 0 and nums[j] == 0) {
+//           j++;
+//         }
+        
+//         if(i<n and j<n and nums[i] == 0 and nums[j] != 0) {
+//           swap(nums[i], nums[j]);
+//           i++;
+//           j++;
+//         } 
+//       }
+      
+      //  using else statement
       while(i<n and j<n) {
         if(nums[i] != 0) {
           i++;
           j++;
-        }
-        if(i<n and j<n and nums[i] == 0 and nums[j] == 0) {
+        }else if (nums[i] == 0 and nums[j] == 0) {
           j++;
-        }
-        if(i<n and j<n and nums[i] == 0 and nums[j] != 0) {
+        }else if(nums[i] == 0 and nums[j] != 0) {
           swap(nums[i], nums[j]);
           i++;
           j++;
