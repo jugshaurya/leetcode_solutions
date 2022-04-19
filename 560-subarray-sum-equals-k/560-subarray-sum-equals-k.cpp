889b7dc9a47689074b56611674c06a77ef716b7a
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-04-19 18:23:39 +0530

    Time: 115 ms (41.33%), Space: 39.2 MB (28.07%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -6,16 +6,26 @@ public:
         vector<int> prefixSum(n, 0);
         map<int, int> mp;
       
-        for(int i=0;i<n;i++){
-          if(i) prefixSum[i] = prefixSum[i-1] + nums[i];
-          else prefixSum[i] = nums[i];
-        }
+//         for(int i=0;i<n;i++){
+//           if(i) prefixSum[i] = prefixSum[i-1] + nums[i];
+//           else prefixSum[i] = nums[i];
+//         }
+        
+//         int ans = 0;
+//         mp[0]++;
+//         for(auto x: prefixSum){
+//           if(mp.count(x-k)) ans+= mp[x-k];
+//           mp[x]++;
+//         }
         
         int ans = 0;
         mp[0]++;
-        for(auto x: prefixSum){
-          if(mp.count(x-k)) ans+= mp[x-k];
-          mp[x]++;
+        for(int i=0;i<n;i++){
+          if(i) prefixSum[i] = prefixSum[i-1] + nums[i];
+          else prefixSum[i] = nums[i];
+          
+          if(mp.count(prefixSum[i]-k)) ans+= mp[prefixSum[i]-k];
+          mp[prefixSum[i]]++;
         }
         
         return ans;
