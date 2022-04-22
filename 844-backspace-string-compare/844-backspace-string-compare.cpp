098838c0b7c305e90f5da12781c16f9444a7ecd0
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-04-22 16:47:55 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (39.15%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -3,25 +3,22 @@ public:
     bool backspaceCompare(string s, string t) {
         int i = 0;
         for(auto x: s){
-          if(x == '#'){
+          if(x == '#') {
             if(i!=0) i--;
-          }else{
-            s[i] = x;
-            i++;
+          } else{
+            s[i++] = x;
           }
         }
-        s = s.substr(0,i);  
+        s = s.substr(0,i);
+      
         i = 0;
         for(auto x: t){
-          if(x == '#'){
-            if(i!=0) i--;
-          }else{
-            t[i] = x;
-            i++;
-          }
+          if(x == '#') {
+            if(i) i--;
+          } else t[i++] = x;
         }
-        t = t.substr(0,i);
       
-        return s==t;
+        t = t.substr(0,i);
+        return s == t;
     }
 };
