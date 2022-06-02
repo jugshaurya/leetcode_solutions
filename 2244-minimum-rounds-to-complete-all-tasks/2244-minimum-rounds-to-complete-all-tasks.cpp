86749bcd0012fde00e28ebbcf67e49db1f51cf54
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-06-02 18:20:00 +0530

    Time: 409 ms (7.82%), Space: 101 MB (91.12%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -1,34 +1,42 @@
+
 vector<int> dp;
 class Solution {
-public:
-    int minRoundsNeeded(int n){
-        if(n<2) return 1e9;
-        if(n == 2) return 1;
-        if(n == 3) return 1;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n] = 1 + min(minRoundsNeeded(n-2), minRoundsNeeded(n-3));
-    }
-    
-    int minimumRounds(vector<int>& tasks) {
-        int n = tasks.size();
-        sort(tasks.begin(), tasks.end());
-        
-        dp = vector<int>(n + 1, -1);
+  public:
+  int minRoundsNeeded(int n) {
+    if (n < 2)
+      return 1e9;
+    if (n == 2)
+      return 1;
+    if (n == 3)
+      return 1;
+    if (dp[n] != -1)
+      return dp[n];
+    return dp[n] = 1 + min(minRoundsNeeded(n - 2), minRoundsNeeded(n - 3));
+  }
 
-        int ans = 0;
-        int cnt = 1;
-        
-        for(int i=1; i<n;i++){
-            if(tasks[i] == tasks[i-1]) cnt++;
-            else{
-                if(cnt==1) return -1;
-                ans += minRoundsNeeded(cnt);
-                cnt = 1;
-            }
-        }
-        
-        if(cnt==1) return -1;
+  int minimumRounds(vector<int>& tasks) {
+    int n = tasks.size();
+    sort(tasks.begin(), tasks.end());
+
+    dp = vector<int>(n + 1, -1);
+
+    int ans = 0;
+    int cnt = 1;
+
+    for (int i = 1; i < n; i++) {
+      if (tasks[i] == tasks[i - 1])
+        cnt++;
+      else {
+        if (cnt == 1)
+          return -1;
         ans += minRoundsNeeded(cnt);
-        return ans;
+        cnt = 1;
+      }
     }
+
+    if (cnt == 1)
+      return -1;
+    ans += minRoundsNeeded(cnt);
+    return ans;
+  }
 };
