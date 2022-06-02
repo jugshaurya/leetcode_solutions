ab0cba7c8cadc096f5c8f3ccc5a1ef09e7b8c104
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-06-02 18:24:36 +0530

    Time: 283 ms (31.17%), Space: 111 MB (5.00%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -3,6 +3,7 @@ vector<int> dp;
 
 class Solution {
   public:
+    
   int minRoundsNeeded(int n) {
     if (n < 2) return 1e9;
     if (n == 2) return 1;
@@ -13,24 +14,20 @@ class Solution {
 
   int minimumRounds(vector<int>& tasks) {
     int n = tasks.size();
-    sort(tasks.begin(), tasks.end());
-
+      
+    // sort(tasks.begin(), tasks.end());
+    map<int, int> m;
+    for (int i = 0; i < n; i++) {
+        m[tasks[i]]++;
+    }
+      
     dp = vector<int>(n + 1, -1);
 
     int ans = 0;
-    int cnt = 1;
-
-    for (int i = 1; i < n; i++) {
-      if (tasks[i] == tasks[i - 1])cnt++;
-      else {
-        if (cnt == 1) return -1;
-        ans += minRoundsNeeded(cnt);
-        cnt = 1;
-      }
+    for(auto [task, freq]: m){
+        if(freq==1) return -1;
+        ans += minRoundsNeeded(freq);
     }
-
-    if (cnt == 1) return -1;
-    ans += minRoundsNeeded(cnt);
     return ans;
   }
 };
