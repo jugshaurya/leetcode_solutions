6943c233c2eb80c03bf6b5e36bbff44ccfca3a11
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-06-02 16:15:00 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (56.37%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -3,27 +3,27 @@ public:
     int climbStairs(int n) {
   
         //=============Bottom up======================
-        vector<int> dp(n+2, 0);
-        dp[1] = 1;
-        dp[2] = 2;
-        for (int i = 3; i<=n; i++){
-           dp[i] = dp[i-1] + dp[i-2];
-        }
+//         vector<int> dp(n+2, 0);
+//         dp[1] = 1;
+//         dp[2] = 2;
+//         for (int i = 3; i<=n; i++){
+//            dp[i] = dp[i-1] + dp[i-2];
+//         }
         
-        return dp[n];
+//         return dp[n];
         
         
         //============= Space Optimized ======================
         
-//         int a = 1, b = 2;
-//         int c = 0 ;
-//         for (int i = 3; i <= n; i++) {
-//             c = a + b;
-//             a = b;
-//             b = c;
-//         }
+        int a = 1, b = 2;
+        int c = 0 ;
+        for (int i = 3; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
         
-//         return c;
+        return n==1 ? 1 : (n==2 ? 2 : c);
       
     }
 };
