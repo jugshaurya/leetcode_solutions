4620eb731372bad5ceab751a7766ab02a4de01ce
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-06-02 11:17:59 +0530

    Time: 6 ms (98.49%), Space: 10.7 MB (13.79%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.cpp
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
         
-        // get a matrix of dimension(n,m) using given matrix of (m,n)     
         int m = matrix.size();
         int n = matrix[0].size();
         
-        vector<vector<int> > mat(n, vector<int>(m,0));
+        // get a matrix of dimension(n,m) using given matrix of (m,n)     
         
+        vector<vector<int> > mat(n, vector<int>(m));
+        //         
         for(int i=0;i<n;i++)
             for(int j=0;j<m;j++)
                 mat[i][j] = matrix[j][i];
