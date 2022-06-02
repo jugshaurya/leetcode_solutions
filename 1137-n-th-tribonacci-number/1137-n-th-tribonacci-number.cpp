3a78079c8e20fc80ad7188f42e1269764a457fcd
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-06-02 16:23:47 +0530

    Time: 4 ms (21.13%), Space: 5.8 MB (83.66%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,30 +1,33 @@
+
 class Solution {
-public:
-    int tribonacci(int n) {
-        
-        if(n < 2) return n;
-        if(n == 2) return 1;
-        
-        //=============Bottom up======================
-        vector<int> dp(n+1, 0);
-        dp[1] = 1;
-        dp[2] = 1;
-        for (int i = 3; i<=n; i++){
-           dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
-        }
-        return dp[n];
+  public:
+  int tribonacci(int n) {
+      
+    //=============Bottom up======================
+    // if (n < 2) return n;
+    // if (n == 2) return 1;
+    // vector<int> dp(n+1, 0);
+    // dp[1] = 1;
+    // dp[2] = 1;
+    // for (int i = 3; i<=n; i++){
+    //    dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+    // }
+    // return dp[n];
 
-        //============= Space Optimized ======================
-
-//         int a = 0, b = 1;
-//         int c = 0;
-//         for (int i = 2; i <= n; i++) {
-//           c = a + b;
-//           a = b;
-//           b = c;
-//         }
-        
-//         if(n<2) return n;
-//         return c; 
+    //============= Space Optimized ======================
+    
+    if (n < 2) return n;
+    if (n == 2) return 1;
+      
+    int a = 0, b = 1, c = 1;
+    int d = 0;
+    for (int i = 3; i <= n; i++) {
+      d = a + b + c;
+      a = b;
+      b = c;
+      c = d;
     }
+    
+    return d;
+  }
 };
