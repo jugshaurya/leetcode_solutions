504b43a2da85c981f9f1eb2b5591111b01098837
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-06-02 20:18:19 +0530

    Time: 201 ms (5.06%), Space: 10.1 MB (61.49%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,17 +1,39 @@
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-     
-        // can use QuickSelect Algorithm
+    
+    int partition(vector<int>& nums, int s, int e) {
         
-        // can use PQ
-        priority_queue<int> pq(nums.begin(), nums.end());
-        k--;
-        while(k--){
-            pq.pop();
+        // TODO partition Fn
+        
+        int pivot = nums[e], i = s;
+        for (int j = s; j <= e - 1; j++) {
+            if (nums[j] <= pivot) {
+                swap(nums[i], nums[j]);
+                i++;
+            }
         }
-        
-        return pq.top();
-            
+        swap(nums[i], nums[e]);
+        return i;
+    }
+    
+    int usingQuickSelect(vector<int>& nums, int k, int s, int e) {
+        int n = nums.size();
+        int idx = partition(nums, s, e);
+        if(idx == n - k) return nums[idx];
+        if(idx < n - k) return usingQuickSelect(nums, k, idx + 1, e);
+        return usingQuickSelect(nums, k, s, idx - 1);
+    }
+    
+    int findKthLargest(vector<int>& nums, int k) {
+        // Method 1: can use QuickSelect Algorithm
+        return usingQuickSelect(nums, k, 0, nums.size()-1);
+
+        // Method 2: can use PQ
+        // priority_queue<int> pq(nums.begin(), nums.end());
+        // k--; 
+        // while(k--){
+        //     pq.pop();
+        // }
+        // return pq.top();
     }
 };
