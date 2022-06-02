2addb025b9494b6d03053c7227ad09fbbb8bac0a
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-06-02 16:09:36 +0530

    Time: 16 ms (17.66%), Space: 13.6 MB (78.96%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -2,19 +2,33 @@ class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
     
-        // recurrence relation
+        // Recurrence Relation
         // dp(i) = min cost to reach ith index
         // dp(i) = min(cost[i-1] + dp(i-1) , cost[i-2] + dp(i-2))
         // dp(0) = 0
         // dp(1) = 0
         // ans is dp(n)
         
+        //=============Bottom up======================
+        // int n = cost.size();
+        // vector<int> dp(n+1, 0);
+        // for (int i = 2; i<=n; i++){
+        //    dp[i] = min(cost[i-1] + dp[i-1] , cost[i-2] + dp[i-2]);
+        // }
+        
+        // return dp[n];
+        
+        
+        //============= Space Optimized ======================
         int n = cost.size();
-        vector<int> dp(n+1, 0);
-        for (int i = 2; i<=n; i++){
-            dp[i] = min(cost[i-1] + dp[i-1] , cost[i-2] + dp[i-2]);
+        int a = 0, b = 0;
+        int c = 0 ;
+        for (int i = 2; i <= n; i++) {
+            c = min(cost[i-1] + b , cost[i-2] + a);
+            a = b;
+            b = c;
         }
         
-        return dp[n];
+        return c;
     }
 };
