deede4420263ff14b2d02868420e0451e74a8802
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-06-02 18:17:21 +0530

    Time: 395 ms (9.21%), Space: 101.2 MB (91.12%) - LeetHub

---
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -0,0 +1,41 @@
+vector<int> dp;
+
+class Solution {
+public:
+    int minRoundsNeeded(int n){
+        if(n<2) return 1e9;
+        if(n == 2) return 1;
+        if(n == 3) return 1;
+        
+        if(dp[n]!=-1) return dp[n];
+        
+        return dp[n] = 1 + min(minRoundsNeeded(n-2), minRoundsNeeded(n-3));
+    }
+    
+    int minimumRounds(vector<int>& tasks) {
+        
+        sort(tasks.begin(), tasks.end());
+        
+        int n = tasks.size();
+        dp = vector<int>(n + 1, -1);
+        
+        int ans = 0;
+        int cnt = 1;
+        for(int i=1; i<n;i++){
+            if(tasks[i]==tasks[i-1]){
+                cnt++;
+            }else{
+                if(cnt==1) return -1;
+                ans += minRoundsNeeded(cnt);
+                cout<<cnt<< " "<<minRoundsNeeded(cnt) <<endl;
+                cnt = 1;
+            }
+        }
+        
+        if(cnt==1) return -1;
+        ans += minRoundsNeeded(cnt);
+        cout<<cnt<< " "<<minRoundsNeeded(cnt) <<endl;
+        
+        return ans;
+    }
+};
