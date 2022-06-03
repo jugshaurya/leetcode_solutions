fd7a6456570b3e9efbb2319f08a4490e0b3eee9c
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-06-03 20:50:11 +0530

    Time: 105 ms (23.27%), Space: 27.3 MB (7.50%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -3,28 +3,50 @@ vector<vector<int>> memo;
 class Solution {
 public:
     
-    // for Method1
-    int LPSHelper(string &s, int i, int j) {
+// ================== Method 1: #insertions = #deletions = n - LPS(s) ==========================
+    
+//     int LPSHelper(string &s, int i, int j) {
+//         if (i > j) return 0;
+//         if (i == j) return 1;
+        
+//         if (memo[i][j] != -1) return memo[i][j];
+        
+//         if (s[i] == s[j]) return memo[i][j] = 2 + LPSHelper(s, i + 1, j - 1);
+//         return memo[i][j] = max(LPSHelper(s, i, j - 1), LPSHelper(s, i + 1, j));
+//     }
+
+//     int LPS(string s) {
+//         int n = s.length();
+//         memo = vector<vector<int>> (n + 1, vector<int>(n + 1, -1));
+//         int ans = LPSHelper(s, 0, s.size() - 1);
+//         return ans;
+//     }
+    
+//     int minInsertions(string s) {
+//         return s.size() - LPS(s);
+//     }
+    
+    
+// ================== Method2: direct dp(i,j) ==========================
+    int rec(string &s, int i, int j) {
         if (i > j) return 0;
-        if (i == j) return 1;
+        if (i == j) return 0;
         
         if (memo[i][j] != -1) return memo[i][j];
         
-        if (s[i] == s[j]) return memo[i][j] = 2 + LPSHelper(s, i + 1, j - 1);
-        return memo[i][j] = max(LPSHelper(s, i, j - 1), LPSHelper(s, i + 1, j));
+        if (s[i] == s[j]) return memo[i][j] = rec(s, i + 1, j - 1);
+        return memo[i][j] = 1 + min(rec(s, i, j - 1), rec(s, i + 1, j));
     }
 
-    // for Method1
-    int LPS(string s) {
+    int helper(string s) {
         int n = s.length();
         memo = vector<vector<int>> (n + 1, vector<int>(n + 1, -1));
-        int ans = LPSHelper(s, 0, s.size() - 1);
-        return ans;
+        return rec(s, 0, s.size() - 1);
     }
     
     int minInsertions(string s) {
-        // Method 1: #insertions = #deletions = n - LPS(s)
-        return s.size() - LPS(s);
+        // Method 2: #insertions = #deletions = n - LPS(s)
+        return helper(s);
     }
 };
 
