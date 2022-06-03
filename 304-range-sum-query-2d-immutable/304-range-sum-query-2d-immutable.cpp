ef179c6b1c80af2175017663bea5387f57b7d3f3
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-06-03 13:32:27 +0530

    Time: 538 ms (64.38%), Space: 151.2 MB (6.77%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -27,14 +27,6 @@ public:
                 preSum[i][j] += preSum[i][j-1] + preSum[i-1][j] - preSum[i-1][j-1];
             }
         }
-        
-        //  print
-        for(int i = 0 ;i < m; i++){
-            for(int j = 0 ;j < n; j++){
-                cout<<preSum[i][j]<<" ";
-            }
-            cout<<endl;
-        }
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
