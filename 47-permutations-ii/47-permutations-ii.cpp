458143176a7e0688aff200e197e020131586ebd8
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-06-03 13:12:25 +0530

    Time: 62 ms (27.60%), Space: 9.7 MB (39.28%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -3,63 +3,63 @@ public:
     
     // =============== Method 1: using `taken` array of indexes ========================
     
-    void helper(vector<int>& nums, vector<int> &taken, vector<int> & order, set<vector<int> >& ans){
+//     void helper(vector<int>& nums, vector<int> &taken, vector<int> & order, set<vector<int> >& ans){
 
-        if(order.size() == nums.size()){
-            ans.insert(order);
-            return;
-        }
+//         if(order.size() == nums.size()){
+//             ans.insert(order);
+//             return;
+//         }
         
-        for(int i=0; i<nums.size(); i++){
-            if(taken[i] == 1) continue;
-            taken[i] = 1;
-            order.push_back(nums[i]);
-            helper(nums, taken, order, ans);
-            taken[i] = 0;
-            order.pop_back();
-        }
+//         for(int i=0; i<nums.size(); i++){
+//             if(taken[i] == 1) continue;
+//             taken[i] = 1;
+//             order.push_back(nums[i]);
+//             helper(nums, taken, order, ans);
+//             taken[i] = 0;
+//             order.pop_back();
+//         }
             
-    }
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<int> taken(nums.size(), 0);
-        set<vector<int>> ans;
-        vector<int> order;
-        
-        helper(nums, taken, order, ans);
-        
-        vector<vector<int> > res;
-        for(auto x: ans){
-            res.push_back(x);
-        }
-        return res;
-    }
-
-    // ===================== Method 2: using swapping ========================
-    
-//     void helper(vector<int>& nums, int start, int end, set<vector<int> >& ans){
-
-//         if(start == end){
-//             ans.insert(nums);
-//             return ;
-//         }
-        
-//         for(int i = start; i<=end; i++){
-//             swap(nums[start], nums[i]);
-//             helper(nums, start + 1, end, ans);
-//             swap(nums[start], nums[i]);
-//         }
 //     }
-    
 //     vector<vector<int>> permuteUnique(vector<int>& nums) {
+//         vector<int> taken(nums.size(), 0);
 //         set<vector<int>> ans;
-//         helper(nums, 0, nums.size()-1 ,ans);
+//         vector<int> order;
+        
+//         helper(nums, taken, order, ans);
         
 //         vector<vector<int> > res;
 //         for(auto x: ans){
 //             res.push_back(x);
 //         }
 //         return res;
-        
 //     }
+
+    // ===================== Method 2: using swapping ========================
+    
+    void helper(vector<int>& nums, int start, int end, set<vector<int> >& ans){
+
+        if(start == end){
+            ans.insert(nums);
+            return ;
+        }
+        
+        for(int i = start; i<=end; i++){
+            swap(nums[start], nums[i]);
+            helper(nums, start + 1, end, ans);
+            swap(nums[start], nums[i]);
+        }
+    }
+    
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        set<vector<int>> ans;
+        helper(nums, 0, nums.size()-1 ,ans);
+        
+        vector<vector<int> > res;
+        for(auto x: ans){
+            res.push_back(x);
+        }
+        return res;
+        
+    }
 
 };
