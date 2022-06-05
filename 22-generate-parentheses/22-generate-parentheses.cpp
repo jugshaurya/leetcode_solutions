accc7e646fcc0320bae2eb20c928bd7d454bf7f8
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-06-05 12:11:49 +0530

    Time: 8 ms (41.12%), Space: 11.4 MB (81.92%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
     
-    void helper(int n, vector<string> &ans, string s="", int openCount = 0, int closeCount = 0){
+    void helper(int n, vector<string> &ans, string &s, int openCount = 0, int closeCount = 0){
         if(openCount==closeCount and openCount==n){
             ans.push_back(s);
             return;
         }    
         
         if(openCount < n){
-            helper(n, ans, s + "(", openCount+1, closeCount);
+            s.push_back('(');
+            helper(n, ans, s, openCount+1, closeCount);
+            s.pop_back();
         }
         
         if(closeCount < openCount){
-            helper(n, ans, s + ")", openCount, closeCount+1);
+            s.push_back(')');
+            helper(n, ans, s , openCount, closeCount+1);
+            s.pop_back();
         }
     }
     
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
-        helper(n, ans);
+        string s;
+        helper(n, ans,s);
         return ans;
     }
 };
