fd1fb0d632f8894a890b42e6cf0f9d33c7e7190f
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-06-07 18:54:57 +0530

    Time: 193 ms (17.57%), Space: 67.7 MB (54.40%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -3,31 +3,31 @@ public:
     int maxSubArray(vector<int>& nums) {
         int n = nums.size();
         
-        // using Kadane's Algo
+        // Method 1: using Kadane's Algo
         // can the subarray considered be empty ? No
         // => Edge Case : If all elements are -ve, return biggest -ve number.
 
-        int biggestNum = nums[0];
-        for(int i=0; i<n; i++){
-            biggestNum = max(biggestNum, nums[i]);
-        }
+//         int biggestNum = nums[0];
+//         for(int i=0; i<n; i++){
+//             biggestNum = max(biggestNum, nums[i]);
+//         }
         
-        if(biggestNum < 0) return biggestNum;
+//         if(biggestNum < 0) return biggestNum;
         
         int currentSum = 0;
         int maxSum = INT_MIN;
-        for (int i=0;i<n;i++){
+        for (int i=0; i<n; i++){
             currentSum += nums[i];
-            if(currentSum < 0) currentSum = 0;
             maxSum = max(maxSum, currentSum);
+            if(currentSum < 0) currentSum = 0;
         }
         
         return maxSum;
         
-        // using DP
+        // using DP - TODO
         
         
-        // using Divide and Conquer
+        // using Divide and Conquer - TODO
         
         
     }
