c2282449ded4ed124013b6b470d39fdeabf7ad49
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-06-07 18:57:55 +0530

    Time: 166 ms (33.58%), Space: 68.1 MB (8.31%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        
+        // Method 1: using Kadane's Algo O(n)
+        // int n = nums.size();
+        // int currentSum = 0;
+        // int maxSum = INT_MIN;
+        // for (int i=0; i<n; i++){
+        //     currentSum += nums[i];
+        //     maxSum = max(maxSum, currentSum);
+        //     if(currentSum < 0) currentSum = 0;
+        // }
+        // return maxSum;
+        
+        // Method 2: using DP - Kadane's is the space optimized version of this solution
         int n = nums.size();
-        
-        // Method 1: using Kadane's Algo
-        // can the subarray considered be empty ? No
-        // => Edge Case : If all elements are -ve, return biggest -ve number.
-
-//         int biggestNum = nums[0];
-//         for(int i=0; i<n; i++){
-//             biggestNum = max(biggestNum, nums[i]);
-//         }
-        
-//         if(biggestNum < 0) return biggestNum;
-        
-        int currentSum = 0;
-        int maxSum = INT_MIN;
-        for (int i=0; i<n; i++){
-            currentSum += nums[i];
-            maxSum = max(maxSum, currentSum);
-            if(currentSum < 0) currentSum = 0;
-        }
-        
-        return maxSum;
-        
-        // using DP - TODO
-        
+        int dp[n+1];
         
         // using Divide and Conquer - TODO
-        
-        
+        int ans = INT_MIN;
+        for(int i=0;i<n;i++){
+            if(i) dp[i] = nums[i] + max(0, dp[i-1]);
+            else dp[i] = nums[i];
+            ans = max(ans,dp[i]);
+        }
+        return ans;
     }
 };
