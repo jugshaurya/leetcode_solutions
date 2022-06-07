66a4d0d10955332c6b5d7c997458cf9eb66d877c
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-06-07 19:41:45 +0530

    Time: 72 ms (78.21%), Space: 41.7 MB (14.01%) - LeetHub

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
@@ -4,11 +4,11 @@ public:
     int kadane(vector<int> &nums){
         int n = nums.size();
         int dp[n+1];
-        int ans = INT_MIN;
+        int ans = INT_MAX;
         for(int i=0;i<n;i++){
-            if(i) dp[i] = nums[i] + max(0, dp[i-1]);
+            if(i) dp[i] = nums[i] + min(0, dp[i-1]);
             else dp[i] = nums[i];
-            ans = max(ans,dp[i]);
+            ans = min(ans,dp[i]);
         }
         return ans;
     }
