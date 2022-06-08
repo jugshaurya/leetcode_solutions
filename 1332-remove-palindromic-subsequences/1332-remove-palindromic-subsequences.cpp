d4b83215965a24418e4c45837b0c493f131c6974
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-06-08 13:30:21 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (26.79%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -2,8 +2,11 @@ class Solution {
 public:
     int removePalindromeSub(string s) {
         string t = s;
+        
         reverse(t.begin(), t.end());
+        // if string is palindrome, onyl one operation is required.
         if(t==s) return 1;
+        // otherwise, in 1 operation delete all a's and in 2nd all b's.
         return 2;
     }
 };
