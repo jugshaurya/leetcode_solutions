178a520d7aecc40717b8081c31b297aa82da0400
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-06-09 18:17:41 +0530

    Time: 2 ms (61.04%), Space: 7 MB (19.68%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,26 +1,35 @@
 class Solution {
-  public:
-  
-  int dp[5005];
-  string s;
-  int ways(int i = 0) {
-    if (i == s.size()) return 1;
-    if (s[i] == '0') return 0;
-    if (i + 1 == s.size() and s[i] == '0') return 0;
-    if (i + 1 == s.size() and s[i] != '0') return 1;
-
-    if (dp[i] != -1) return dp[i];
-
-    int ans = 0;
-    if (s[i] >= '1' and s[i] <= '9') ans += ways(i + 1);
-    if (s[i] == '1') ans += ways(i + 2);
-    if (i + 1 < s.size() and (s[i] == '2' and s[i + 1] <= '6')) ans += ways(i + 2);
-    return dp[i] = ans;
-  }
-
-  int numDecodings(string str) {
-    s = str;
-    memset(dp, -1, sizeof(dp));
-    return ways();
-  }
+public:
+    vector<int> dp;
+    
+    int helper(string s) { 
+        int n = s.size();
+        // Case: 0 length
+        if(n == 0) return 1;
+        
+        // case: 1 length
+        if(n == 1 and s[0] == '0') return 0; 
+        if(n == 1) return 1; 
+        
+        // Case: atleast string is 2 length
+        if(s[0] == '0') return 0;
+        // Memoization
+        if(dp[n] != -1) return dp[n];
+        
+        int ans = helper(s.substr(1));
+        int num = (s[0]-'0')*10 + (s[1]-'0');
+        
+        if(num >= 1 and num <= 26){
+            ans += helper(s.substr(2));
+        }
+        
+        return dp[n] = ans;
+        
+    }
+    
+    int numDecodings(string s) {
+        int n = s.size();
+        dp.assign(n + 1, -1);
+        return helper(s);
+    }
 };
