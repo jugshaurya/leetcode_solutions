c2a2255269886f3e3e4727b306abbde544eef480
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-06-13 18:39:33 +0530

    Time: 4 ms (76.92%), Space: 6.2 MB (40.76%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -1,42 +1,28 @@
 class Solution {
-public:
-  
-  bool check(int n, int i, int j, vector<vector<int> >& board) {
-    // check same column
-    for (int ii = i - 1; ii >= 0; ii--) 
-      if (board[ii][j] == 1) return false;
+  public:
+  vector<int> col;
+  vector<int> lDiagonal;
+  vector<int> rDiagonal;
 
-    // check LeftDiagonal
-    int ii = i - 1;
-    int jj = j - 1;
-    while (ii >= 0 and jj >= 0) {
-      if (board[ii][jj]) return false;
-      ii--;
-      jj--;
-    }
-
-    // check rightDiagonal
-    ii = i - 1;
-    jj = j + 1;
-    while (ii >= 0 and jj < n) {
-      if (board[ii][jj]) return false;
-      ii--;
-      jj++;
-    }
-    return true;
+  bool check(int n, int r, int c) {
+    return col[c] == 0 and lDiagonal[r - c + n - 1] == 0 and rDiagonal[r + c] == 0;
   }
 
-  int ways(int n, int i, vector<vector<int> >&board) {
+  int ways(int n, int i) {
     if (i == n) {
       return 1;
     }
 
     int ans = 0;
     for (int j = 0; j < n; j++) {
-      if (check(n, i, j, board)) {
-        board[i][j] = 1;
-        ans += ways(n, i + 1, board);
-        board[i][j] = 0;
+      if (check(n, i, j)) {
+        col[j] = 1;
+        lDiagonal[i-j + n - 1] = 1;
+        rDiagonal[i+j] = 1;
+        ans += ways(n, i + 1);
+        col[j] = 0;
+        lDiagonal[i-j + n - 1] = 0;
+        rDiagonal[i+j] = 0;
       }
     }
 
@@ -44,7 +30,11 @@ public:
   }
 
   int totalNQueens(int n) {
-    vector<vector<int>> board(n, vector<int> (n,0));
-    return ways(n, 0, board);
+    vector<vector<int>> board(n, vector<int>(n, 0));
+    col = vector<int>(n, 0);
+    // r - c (range [-(n-1), n-1]) => always add n-1 to normalize
+    lDiagonal = vector<int>(2 * n - 1, 0);
+    rDiagonal = vector<int>(2 * n - 1, 0);  // r + c (range [0, n-1 + n-1])
+    return ways(n, 0);
   }
 };
