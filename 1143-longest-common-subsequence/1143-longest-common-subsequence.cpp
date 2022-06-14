299f48538cf8ae4cea5f2a7399f71d034ced55b4
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-06-14 16:17:32 +0530

    Time: 70 ms (7.51%), Space: 17.5 MB (10.51%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,34 +1,35 @@
 class Solution {
-    public:
-    int memo[1005][1005];
-    int LCS(string &a, string &b, int i = 0, int j = 0) {
-        int A = a.size();
-        int B = b.size();
-        if (i == A) return 0;
-        if (j == B) return 0;
-        if (memo[i][j] != -1) return memo[i][j];
-        if (a[i] == b[j]) return memo[i][j] = 1 + LCS(a, b, i + 1, j + 1);
-        return memo[i][j] = max(LCS(a, b, i + 1, j), LCS(a, b, i, j + 1));
-    }
+  public:
+  int memo[1005][1005];
+  int LCS(string &a, string &b, int i = 0, int j = 0) {
+    int A = a.size();
+    int B = b.size();
+      
+    if (i == A) return 0;
+    if (j == B) return 0;
+      
+    if (memo[i][j] != -1) return memo[i][j];
+    
+    if (a[i] == b[j]) return memo[i][j] = 1 + LCS(a, b, i + 1, j + 1);
+    return memo[i][j] = max(LCS(a, b, i + 1, j), LCS(a, b, i, j + 1));
+  }
 
-    int LCSIterative(string &a, string &b) {
-        int n = a.length();
-        int m = b.length();
+  int LCSIterative(string &a, string &b) {
+    int n = a.length();
+    int m = b.length();
 
-        int memo[n + 1][m + 1];
-        for (int i = 0; i <= n; i++) memo[i][0] = 0;
-        for (int i = 0; i <= m; i++) memo[0][i] = 0;
-        for (int i = 1; i <= n; i++)
-            for (int j = 1; j <= m; j++)
-                if (a[i - 1] == b[j - 1])
-                    memo[i][j] = 1 + memo[i - 1][j - 1];
-                else
-                    memo[i][j] = max(memo[i][j - 1], memo[i - 1][j]);
-        return memo[n][m];
-    }
+    vector<vector<int> > memo(n + 1, vector<int>(m + 1, 0));
+      
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++)
+            if (a[i - 1] == b[j - 1]) memo[i][j] = 1 + memo[i - 1][j - 1];
+            else memo[i][j] = max(memo[i][j - 1], memo[i - 1][j]);
+      
+    return memo[n][m];
+  }
 
-    int longestCommonSubsequence(string text1, string text2) {
-        memset(memo, -1, sizeof(memo));
-        return LCS(text1, text2);
-    }
+  int longestCommonSubsequence(string text1, string text2) {
+    memset(memo, -1, sizeof(memo));
+    return LCSIterative(text1, text2);
+  }
 };
