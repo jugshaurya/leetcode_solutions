e1b2469c7875865e5ed988f4de1507abb49e9285
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-06-18 13:03:32 +0530

    Time: 39 ms (31.85%), Space: 14.5 MB (35.32%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -7,40 +7,103 @@ public:
     // can be done using DFS, check for the stack presence of a node.
     
     // Method 1: using BFS
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+//     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         
-        vector<vector<int>> gr(numCourses);
-        vector<int> indegree(numCourses, 0);
+//         vector<vector<int>> gr(numCourses);
+//         vector<int> indegree(numCourses, 0);
 
-        for(auto x: prerequisites){
-            gr[x[1]].push_back(x[0]);
-            indegree[x[0]]++;
-        }
+//         for(auto x: prerequisites){
+//             gr[x[1]].push_back(x[0]);
+//             indegree[x[0]]++;
+//         }
         
-        queue<int> q;
-        for(int i=0;i<numCourses;i++){
-            if(indegree[i]==0) q.push(i);
-        }
+//         queue<int> q;
+//         for(int i=0;i<numCourses;i++){
+//             if(indegree[i]==0) q.push(i);
+//         }
         
-        while(!q.empty()){
-            int f = q.front(); q.pop();
-            for(auto nbr: gr[f]){
-                indegree[nbr]--;
-                if(indegree[nbr]==0) q.push(nbr);
+//         while(!q.empty()){
+//             int f = q.front(); q.pop();
+//             for(auto nbr: gr[f]){
+//                 indegree[nbr]--;
+//                 if(indegree[nbr]==0) q.push(nbr);
+//             }
+//         }
+        
+//         for(auto x: indegree){
+//             if(x != 0) return false;
+//         }
+        
+//         return true;
+//     }
+
+    // Method 2: using DFS
+    bool containsCycle(vector<vector<int>>& gr, vector<int> &visited, int src){
+        visited[src] = 2;
+        
+        for(auto nbr: gr[src]) {
+            if(visited[nbr] == 0) {
+                // we got a forward edge
+                if(containsCycle(gr, visited, nbr)) return true;
+            }else if (visited[nbr] == 1){
+                // we got a cross edge
+                continue;
+            }else {
+                // we got a backedge
+                return true;
             }
         }
+        visited[src] = 1;
+        return false;
+    }
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+
+        vector<vector<int>> gr(numCourses);
+        for(auto x: prerequisites){
+            gr[x[1]].push_back(x[0]);
+        }
+
+        vector<int> visited(numCourses, 0); // states: {0, 1, 2} 
+        // 0 means not visited yet
+        // 1 means visited and not in current recursion stack.
+        // 2 means visited and is in current recursion stack.
         
-        for(auto x: indegree){
-            if(x != 0) return false;
+        for(int i=0; i < numCourses; i++){
+            if(visited[i] == 0 and containsCycle(gr, visited, i)) return false;
         }
         
         return true;
-        
-        
     }
-
-    // Method 2: using DFS
-//     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        
-//     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
