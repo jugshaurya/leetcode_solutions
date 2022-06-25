6de2e1bf8bfc597a7ee609c034729cd0538291c7
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-06-25 18:57:38 +0530

    Time: 18 ms (73.16%), Space: 19.8 MB (43.06%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,25 +1,60 @@
+// Method 2: Without extra space, Two Pointers
+// // O(n) Time
+// // O(1) space
 class Solution {
 public:
     int trap(vector<int>& height) {
-        
         int n = height.size();
-        vector<int> lmax(n), rmax(n);
+        int i = 0;
+        int j = n - 1;
         
-        for(int i=0; i<n; i++){
-            if(i) lmax[i] = max(lmax[i-1], height[i]);
-            else lmax[i] = height[i];
-        }
+        int lmax = -1;
+        int rmax = -1;
         
-        for(int i=n-1; i>=0; i--){
-            if(i != n-1) rmax[i] = max(rmax[i+1], height[i]);
-            else rmax[i] = height[i];
-        }
-        
-        int ans =0 ;
-        for(int i=0; i<n; i++){
-            ans +=  min(lmax[i], rmax[i])- height[i];
+        int ans = 0;
+        while(i <= j) {
+            lmax = max(lmax, height[i]);
+            rmax = max(rmax, height[j]);
+            
+            if(height[i] < height[j]){
+                ans += lmax - height[i];
+                i++;
+            }else{
+                ans += rmax - height[j];
+                j--;
+            }
         }
         
         return ans;
     }
 };
+
+// Method 1: using extra space
+// // O(n) Time
+// // O(n) space
+// class Solution {
+// public:
+//     int trap(vector<int>& height) {
+        
+//         int n = height.size();
+//         vector<int> lmax(n), rmax(n);
+        
+//         for(int i=0; i<n; i++){
+//             if(i) lmax[i] = max(lmax[i-1], height[i]);
+//             else lmax[i] = height[i];
+//         }
+        
+//         for(int i=n-1; i>=0; i--){
+//             if(i != n-1) rmax[i] = max(rmax[i+1], height[i]);
+//             else rmax[i] = height[i];
+//         }
+        
+//         int ans =0 ;
+//         for(int i=0; i<n; i++){
+//             int minBuilding
+//             ans +=  min(lmax[i], rmax[i]) - height[i];
+//         }
+        
+//         return ans;
+//     }
+// };
