693278e1472183ccd41bd1cd8992c181bf2d7109
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-06-25 20:16:22 +0530

    Time: 112 ms (73.55%), Space: 59 MB (80.99%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
+    
+    // using maths 
+    // we want to maximize the expression
+    // exp = (j-i)*(min(height[i], height[j]));
+    // (j-i), will always be decreasing.as we move from i and j toward each other.
+    // but we can maximize `min(height[i], height[j])` if we take keep on discarding the smaller among two.
+    // Why? because, smaller one among both is the one that is reducing the expression value.
+    
     int maxArea(vector<int>& height) {
-      int n = height.size();
-      int s = 0;
-      int e = n-1;
-      int ans = 0;
-      while(s < e){
-        ans = max(ans, (e-s)*min(height[s], height[e]));
-        if(height[s]< height[e]) s++;
-        else e--;
-      }
-      return ans;
+        int n = height.size();
+        int i = 0;
+        int j = n-1;
+        
+        int ans = 0;
+        
+        // dont take `=` because, we dont want area formed by single building. 
+        while(i < j) {
+            ans = max(ans, (j-i)*(min(height[j], height[i])));
+            if(height[i] < height[j])  i++;
+            else j--;
+        }
+        
+        
+        return ans;
     }
 };
