828a39f89e4aee30287b231c6aded09b74f08310
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-06-26 22:52:22 +0530

    Time: 326 ms (85.61%), Space: 134.8 MB (50.60%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -4,22 +4,20 @@ public:
         int n = nums.size();
         deque<int> dq;
         
-        int i = 0;
-        for(; i < k; i++) {
+        for(int i = 0; i < k; i++) {
             while(!dq.empty() and nums[dq.back()] <= nums[i]) dq.pop_back();
             dq.push_back(i); 
         }
 
         vector<int> res;
-        res.push_back(nums[dq.front()]);
         
         for(int i = k; i < n; i++) {
+            res.push_back(nums[dq.front()]);
             if(!dq.empty() and dq.front() < i - k + 1) dq.pop_front();
             while(!dq.empty() and nums[dq.back()] <= nums[i]) dq.pop_back();
             dq.push_back(i);
-            res.push_back(nums[dq.front()]);
         }
-        
+        res.push_back(nums[dq.front()]);
         return res;
     }
 };
