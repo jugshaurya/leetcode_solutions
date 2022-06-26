d581560bc598f454d690e8ec89b9559a7036fae4
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-06-26 22:52:34 +0530

    Time: 322 ms (86.78%), Space: 134.6 MB (65.35%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -10,14 +10,14 @@ public:
         }
 
         vector<int> res;
+        res.push_back(nums[dq.front()]);
         
         for(int i = k; i < n; i++) {
-            res.push_back(nums[dq.front()]);
             if(!dq.empty() and dq.front() < i - k + 1) dq.pop_front();
             while(!dq.empty() and nums[dq.back()] <= nums[i]) dq.pop_back();
             dq.push_back(i);
+            res.push_back(nums[dq.front()]);
         }
-        res.push_back(nums[dq.front()]);
         return res;
     }
 };
