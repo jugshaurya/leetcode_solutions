fb8485603fda05d7e309bf28515f6f5bd7da8cc1
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-06-28 20:30:55 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (70.25%) - LeetHub

---
+++ 342-power-of-four/342-power-of-four.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n == 0) return false;
+        if(n == INT_MIN) return false;
+        
+        // should be a power of 2 as well
+        // and set bit should be @odd place
+        
+        return ((n&(n-1))== 0) and (n&(0x55555555))!=0;
+    }
+};
