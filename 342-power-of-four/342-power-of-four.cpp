4bfab10e02f75e507710bdc5b929fca804cd373d
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-06-28 20:32:25 +0530

    Time: 3 ms (56.94%), Space: 5.7 MB (99.18%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -7,8 +7,8 @@ public:
         // should be a power of 2 as well
         // and set bit should be @odd place
         
-        return (n&(n-1)) == 0 and (n&(0xAAAAAAAA))==0;
+        // return (n&(n-1)) == 0 and (n&(0xAAAAAAAA)) == 0;
         // or
-        // return ((n&(n-1))== 0) and (n&(0x55555555))!=0;
+        return (n&(n-1)) == 0 and (n&(0x55555555)) != 0;
     }
 };
