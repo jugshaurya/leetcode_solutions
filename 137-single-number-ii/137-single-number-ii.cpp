0aa171b6500e4d0eda0b78fe4424a29fa63069bb
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-06-28 12:26:04 +0530

    Time: 12 ms (60.70%), Space: 9.5 MB (86.58%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.cpp
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -1,20 +1,42 @@
+// O(1) space
+// O(32n) time
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        vector<int> bitsArray(32, 0);
-        
-        for(auto x: nums){
-            for(int i = 0; i < 32; i++) {
-                bitsArray[i] += ((x >> i) & 1); 
-                bitsArray[i] %= 3; 
-            }
-        }
         
         int ans = 0;
-        for(int i = 0; i < 32; i++){
-            ans |= bitsArray[i] << i; 
+        for(int i = 0; i < 32; i++) {
+            int bit = 0;
+            for(auto x: nums){
+                bit += ((x >> i) & 1); 
+                bit %= 3;
+            }
+            ans |= (bit << i);
         }
         
         return ans;
     }
 };
+
+// // O(32) space
+// // O(32n) time
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& nums) {
+//         vector<int> bitsArray(32, 0);
+        
+//         for(auto x: nums){
+//             for(int i = 0; i < 32; i++) {
+//                 bitsArray[i] += ((x >> i) & 1); 
+//                 bitsArray[i] %= 3; 
+//             }
+//         }
+        
+//         int ans = 0;
+//         for(int i = 0; i < 32; i++){
+//             ans |= bitsArray[i] << i; 
+//         }
+        
+//         return ans;
+//     }
+// };
