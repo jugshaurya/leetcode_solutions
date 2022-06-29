d52c0e15e057d1afe70eecadb62fd84cf71788af
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-06-29 13:13:07 +0530

    Time: 9 ms (78.98%), Space: 10.1 MB (36.73%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,33 +1,57 @@
-// Method 1: can use QuickSelect Algorithm O(n*n)
-// In worst Case: T(n) =  T(n-1)  + O(n) 
+
+// Method 3: using PQ of size k, O(k + (n-k)logk)
+// answer every query in O(1)
+// optimized for answering queries/for stream of data.
 class Solution {
 public:
-    int partition(vector<int>& nums, int s, int e) {
-        int pivot = nums[e];
-        int i = s;
-        for (int j = s; j <= e - 1; j++) 
-            if (nums[j] <= pivot) swap(nums[i++], nums[j]);
-        swap(nums[i], nums[e]);
-        return i;
-    }
-    
-    int usingQuickSelect(vector<int>& nums, int k, int s, int e) {
-        int n = nums.size();
-        if(s > e) return -1;
-
-        int idx = partition(nums, s, e);
-        
-        if(idx == n - k) return nums[idx];
-        if(idx < n - k) return usingQuickSelect(nums, k, idx + 1, e);
-        else return usingQuickSelect(nums, k, s, idx - 1);
-    }
-    
     int findKthLargest(vector<int>& nums, int k) {
-        return usingQuickSelect(nums, k, 0, nums.size()-1);
+        int n = nums.size();
+        priority_queue<int, vector<int>, greater<int> > pq; 
+        for(int i=0; i<k; i++) pq.push(nums[i]);
+        
+        for(int i=k; i<n; i++){
+            if(nums[i]>pq.top()){
+                pq.pop();
+                pq.push(nums[i]);
+            }
+        }
+        
+        return pq.top();
     }
 };
 
-// Method 2: can use PQ O(n + klogn)
+
+// Method 2: can use QuickSelect Algorithm O(n*n)
+// In worst Case: T(n) =  T(n-1)  + O(n) 
+// class Solution {
+// public:
+//     int partition(vector<int>& nums, int s, int e) {
+//         int pivot = nums[e];
+//         int i = s;
+//         for (int j = s; j <= e - 1; j++) 
+//             if (nums[j] <= pivot) swap(nums[i++], nums[j]);
+//         swap(nums[i], nums[e]);
+//         return i;
+//     }
+    
+//     int usingQuickSelect(vector<int>& nums, int k, int s, int e) {
+//         int n = nums.size();
+//         if(s > e) return -1;
+
+//         int idx = partition(nums, s, e);
+        
+//         if(idx == n - k) return nums[idx];
+//         if(idx < n - k) return usingQuickSelect(nums, k, idx + 1, e);
+//         else return usingQuickSelect(nums, k, s, idx - 1);
+//     }
+    
+//     int findKthLargest(vector<int>& nums, int k) {
+//         return usingQuickSelect(nums, k, 0, nums.size()-1);
+//     }
+// };
+
+// Method 1: can use PQ O(n + klogn)
+// not optimized for answering queries/ for stream of data insertion
 // class Solution {
 // public:
 //     int findKthLargest(vector<int>& nums, int k) {
@@ -36,4 +60,3 @@ public:
 //         return pq.top();
 //     }
 // };
-
