9b2d194430fdca029dc56fc1910f407396f714a9
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-06-29 12:59:01 +0530

    Time: 148 ms (6.47%), Space: 10 MB (61.46%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,37 +1,43 @@
-// Method 1: can use QuickSelect Algorithm O(n^2)
+// Method 1: can use QuickSelect Algorithm O(n*n)
 // In worst Case: T(n) =  T(n-1)  + O(n) 
-// class Solution {
-// public:
-//     int partition(vector<int>& nums, int s, int e) {
-//         int pivot = nums[e];
-//         int i = s;
-//         for (int j = s; j <= e - 1; j++) 
-//             if (nums[j] <= pivot) swap(nums[i++], nums[j]);
-//         swap(nums[i], nums[e]);
-//         return i;
-//     }
-    
-//     int usingQuickSelect(vector<int>& nums, int k, int s, int e) {
-//         int n = nums.size();
-//         int idx = partition(nums, s, e);
-//         if(idx == n - k) return nums[idx];
-//         if(idx < n - k) return usingQuickSelect(nums, k, idx + 1, e);
-//         return usingQuickSelect(nums, k, s, idx - 1);
-//     }
-    
-//     int findKthLargest(vector<int>& nums, int k) {
-//         return usingQuickSelect(nums, k, 0, nums.size()-1);
-//     }
-// };
-
-// Method 2: can use PQ O(n + klogn)
 class Solution {
 public:
+    int partition(vector<int>& nums, int s, int e) {
+        int pivot = nums[e];
+        int i = s;
+        int j = s;
+        while(j <= e){
+            if(nums[j] <= pivot){
+                swap(nums[i++], nums[j]);
+            }
+            j++;
+        }
+        return i-1;
+    }
+    
+    int usingQuickSelect(vector<int>& nums, int k, int s, int e) {
+        int n = nums.size();
+        if(s > e) return -1;
+
+        int idx = partition(nums, s, e);
+        
+        if(idx == n - k) return nums[idx];
+        if(idx < n - k) return usingQuickSelect(nums, k, idx + 1, e);
+        else return usingQuickSelect(nums, k, s, idx - 1);
+    }
+    
     int findKthLargest(vector<int>& nums, int k) {
-        // O(n)
-        priority_queue<int> pq(nums.begin(), nums.end());
-        while(--k) pq.pop();
-        return pq.top();
+        return usingQuickSelect(nums, k, 0, nums.size()-1);
     }
 };
+
+// Method 2: can use PQ O(n + klogn)
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         priority_queue<int> pq(nums.begin(), nums.end());
+//         while(--k) pq.pop();
+//         return pq.top();
+//     }
+// };
 
