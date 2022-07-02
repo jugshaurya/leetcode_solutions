3cd2c61559c7a1263cdd8d841f28aa45a9c6c2ae
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-07-02 18:05:41 +0530

    Time: 624 ms (57.37%), Space: 174.2 MB (36.10%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,3 +1,4 @@
+// Method2: using STL's list
 class LRUCache {
 public:
     int capacity;
@@ -53,3 +54,110 @@ public:
         }
     }
 };
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
+
+// Method 1: using list from scratch
+// will give TLE.
+// struct Node{
+//     int key;
+//     int value;
+//     Node* next;
+//     Node* prev;
+//     Node(int key, int value): key(key), value(value), next(NULL), prev(NULL){}
+// };
+
+// class LRUCache {
+// public:
+    
+//     Node* head;
+//     Node* tail;
+//     int capacity;
+//     int sz;
+//     map<int, pair<int, Node*>> mp;
+    
+//     LRUCache(int capacity) {
+//         this->capacity = capacity;  
+//         head = tail = NULL;
+//         sz = 0;
+//     }
+    
+//     void shiftNodeToFront(Node* node) {
+//         if(node->prev == NULL) return;
+//         if(node->next == NULL) {
+//             tail = node->prev;
+//             node->prev->next = NULL;
+//             node->prev = NULL;
+//             node->next = head;
+//             head->prev = node;
+//             head = node;
+//             return;
+//         }
+        
+//         node->prev->next = node->next;
+//         node->next->prev = node->prev;
+//         node->next = head;
+//         head->prev = node;
+//         node->prev = NULL;
+//         head = node;
+//     }
+    
+//     int get(int key) {
+//         if(mp.count(key) == 0) return -1;
+//         shiftNodeToFront(mp[key].second);
+        
+//         return head->value;
+//     }
+    
+//     // put in front if used latest
+//     // => least recenlty used will be at back
+//     void put(int key, int value) {
+//         if(mp.count(key) == 1) {
+//             shiftNodeToFront(mp[key].second);
+//             mp[key].first = value;
+//             mp[key].second->value = value;
+//             return;
+//         }
+        
+//         if(sz == capacity){
+//             // remove the last Node
+//             Node* tmp = tail;
+//             if(tail->prev == NULL) {
+//                 head = tail = NULL;
+//             }else{
+//                 tail = tail->prev;
+//                 tail->next = NULL;
+//             }
+//             mp.erase(tmp->key);
+//             delete tmp;
+//             // put the new Node at front and in map
+//             Node* newNode = new Node(key, value);
+//             if(head == NULL){
+//                 head = tail = newNode;
+//             }else{
+//                 newNode->next = head;
+//                 head->prev = newNode;
+//                 head = newNode;
+//             }
+//             mp[key] = {value, newNode};
+//         }else{
+//             // we have space to add in cache
+//             // push into the front
+//             sz++;
+//             Node* newNode = new Node(key, value);
+//             if(head == NULL) {
+//                 head = tail = newNode;
+//             }else{
+//                 newNode->next = head;
+//                 head->prev = newNode;
+//                 head = newNode;
+//             }
+//             mp[key] = {value, newNode};
+//         }
+//     }
+// };
