456a6b2138035229bba712dd7954f52fc83d5244
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-07-04 14:31:42 +0530

    Time: 8 ms (41.49%), Space: 12.5 MB (61.61%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
@@ -9,6 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// Method 1: usign NULL as marker
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
@@ -17,19 +19,48 @@ public:
         vector<vector<int>> ans;
         queue<TreeNode*> q;
         q.push(root);
-        
+        q.push(NULL);
+
+        vector<int> tmp;
         while(!q.empty()) {
-            int len = q.size();
-            vector<int> tmp;
-            while(len--){
-                auto front = q.front(); q.pop();
+            auto front = q.front(); q.pop();
+            if(front == NULL) {
+                if(!q.empty()) q.push(NULL);
+                ans.push_back(tmp);
+                tmp.clear();
+            }else{
                 tmp.push_back(front->val);
                 if(front->left) q.push(front->left);
                 if(front->right) q.push(front->right);
             }
-            ans.push_back(tmp);            
         }
         
         return ans;
     }
 };
+
+// Method 2: Using while loop inside while loop
+// class Solution {
+// public:
+//     vector<vector<int>> levelOrder(TreeNode* root) {
+//         if(root == NULL) return {};
+        
+//         vector<vector<int>> ans;
+//         queue<TreeNode*> q;
+//         q.push(root);
+        
+//         while(!q.empty()) {
+//             int len = q.size();
+//             vector<int> tmp;
+//             while(len--){
+//                 auto front = q.front(); q.pop();
+//                 tmp.push_back(front->val);
+//                 if(front->left) q.push(front->left);
+//                 if(front->right) q.push(front->right);
+//             }
+//             ans.push_back(tmp);            
+//         }
+        
+//         return ans;
+//     }
+// };
