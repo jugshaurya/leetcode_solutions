90823a5dfaaf6e9ee2a4a4d283159773b88d533d
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-07-05 17:56:04 +0530

    Time: 26 ms (71.06%), Space: 26.3 MB (47.37%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -9,14 +9,16 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// Method2: Optimized
 class Solution {
 public:
-    //  To make searching for index O(1)   
+    //  To make searching for index O(1)  
+    // instead of using O(n) to search for the partition
     map<int, int> inorderMap;
-    
     TreeNode* buildTreeHelper(vector<int>& preorder, vector<int>& inorder, int preS, int preE, int inS, int inE) {
         
-        if(inS > inE) return NULL;
+        if(preS > preE) return NULL;
         
         TreeNode* root = new TreeNode(preorder[preS]);
         // search in O(1) using map.
@@ -30,7 +32,37 @@ public:
     
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         int n = inorder.size();
-        for(int i=0;i<n;i++) inorderMap[inorder[i]] = i;
+        for(int i = 0; i<n; i++) inorderMap[inorder[i]] = i;
         return buildTreeHelper(preorder, inorder, 0, n-1, 0, n-1);
     }
 };
+
+// Method1: creating new arrays(extra memory)
+// + using O(n) to search for the partition
+// class Solution {
+// public:
+    
+//     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+//         if(preorder.size()==0 || inorder.size()==0) return NULL;
+        
+//         int i=0;
+//         for(; i<(int)(inorder.size()); i++){
+//             if(inorder[i] == preorder[0]) break;
+//         }
+//         // cout<<i<<endl;
+        
+//         vector<int> leftPreorder(preorder.begin() + 1, preorder.begin() + i+1);
+//         vector<int> rightPreorder(preorder.begin() + i+1, preorder.end());
+//         vector<int> leftInorder(inorder.begin(), inorder.begin() + i);
+//         vector<int> rightInorder(inorder.begin() + i + 1, inorder.end());
+//         // cout<<leftPreorder.size()<<"d";
+//         // cout<<rightPreorder.size()<<"d";
+//         // cout<<leftInorder.size()<<"d";
+//         // cout<<rightInorder.size()<<"d";
+//         TreeNode* root  = new TreeNode(preorder[0]);
+//         root->left = buildTree(leftPreorder, leftInorder);
+//         root->right = buildTree(rightPreorder, rightInorder);
+        
+//         return root;
+//     }
+// };
