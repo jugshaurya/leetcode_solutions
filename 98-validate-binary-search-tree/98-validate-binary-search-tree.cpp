7071c0362442fcfbf4ed955a1f0769075723ae70
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-07-05 15:42:58 +0530

    Time: 12 ms (85.50%), Space: 21.6 MB (70.27%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -10,28 +10,49 @@
  * };
  */
 
-struct ReturnIsBST{
-  bool isBST;
-  long long maxVal;
-  long long minVal;
-};
+// Method 1
+// struct ReturnIsBST{
+//   bool isBST;
+//   long long maxVal;
+//   long long minVal;
+// };
 
+// class Solution {
+// public:
+//     ReturnIsBST isValidBSTHelper(TreeNode* root){
+//       if(root == NULL) return {true, LONG_MIN, LONG_MAX};
+    
+//       ReturnIsBST leftAns = isValidBSTHelper(root->left);
+//       ReturnIsBST rightAns = isValidBSTHelper(root->right);
+      
+//       bool a = leftAns.isBST and rightAns.isBST and root->val > leftAns.maxVal and root->val < rightAns.minVal;
+//       long long b = max({leftAns.maxVal, rightAns.maxVal, (long long)root->val}); 
+//       long long c = min({leftAns.minVal, rightAns.minVal, (long long)root->val}); 
+      
+//       return {a,b,c}; 
+//     }
+  
+//     bool isValidBST(TreeNode* root) {
+//       return isValidBSTHelper(root).isBST;
+//     }
+// };
+
+// Method 2
 class Solution {
 public:
-    ReturnIsBST isValidBSTHelper(TreeNode* root){
-      if(root == NULL) return {true, LONG_MIN, LONG_MAX};
     
-      ReturnIsBST leftAns = isValidBSTHelper(root->left);
-      ReturnIsBST rightAns = isValidBSTHelper(root->right);
-      
-      bool a = leftAns.isBST and rightAns.isBST and root->val > leftAns.maxVal and root->val < rightAns.minVal;
-      long long b = max({leftAns.maxVal, rightAns.maxVal, (long long)root->val}); 
-      long long c = min({leftAns.minVal, rightAns.minVal, (long long)root->val}); 
-      
-      return {a,b,c}; 
+    bool isValidBSTHelper(TreeNode* root, long leftLimit = LONG_MIN, long rightLimit = LONG_MAX){
+        if(root == NULL) return true;
+        
+        if(root->val > leftLimit and root->val < rightLimit) {
+            return isValidBSTHelper(root->left, leftLimit, root->val) and isValidBSTHelper(root->right, root->val, rightLimit);
+        }
+        
+        return false;
+        
     }
   
     bool isValidBST(TreeNode* root) {
-      return isValidBSTHelper(root).isBST;
+      return isValidBSTHelper(root);
     }
 };
