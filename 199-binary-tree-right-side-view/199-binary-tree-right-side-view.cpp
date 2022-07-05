85109629780da2b2cf37faa415fe97c7e614d0fb
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-07-05 21:02:51 +0530

    Time: 7 ms (45.63%), Space: 12 MB (44.63%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -11,35 +11,48 @@
  */
 
 // Method1: using Level order traversal(BFS)
-class Solution {
-public:
-    vector<int> rightSideView(TreeNode* root) {
-        if(root == NULL) return {};
-        
-        queue<TreeNode*> q;
-        q.push(root);
-        
-        vector<int> ans;
-        while(!q.empty()){
-            int len = q.size();
-            while(len--){
-                auto front = q.front(); q.pop();
-                if(len == 0) ans.push_back(front->val);
-                if(front->left) q.push(front->left);
-                if(front->right) q.push(front->right);
-            }
-        }
-        
-        return ans;
-    }
-};
-
-
-
-// Method2: using Reverse of preorder (DFS)
 // class Solution {
 // public:
 //     vector<int> rightSideView(TreeNode* root) {
+//         if(root == NULL) return {};
         
+//         queue<TreeNode*> q;
+//         q.push(root);
+        
+//         vector<int> ans;
+//         while(!q.empty()){
+//             int len = q.size();
+//             while(len--){
+//                 auto front = q.front(); q.pop();
+//                 if(len == 0) ans.push_back(front->val);
+//                 if(front->left) q.push(front->left);
+//                 if(front->right) q.push(front->right);
+//             }
+//         }
+        
+//         return ans;
 //     }
 // };
+
+// Method2: using Root, Right, Left Traversal (DFS)
+class Solution {
+public:
+    void helper(TreeNode* root, int& maxLevelSeenSoFar, int level, vector<int> &ans) {
+        if(root == NULL) return;
+        
+        if(level > maxLevelSeenSoFar) {
+            ans.push_back(root->val);
+            maxLevelSeenSoFar = level;
+        };
+        
+        helper(root->right, maxLevelSeenSoFar, level + 1, ans);
+        helper(root->left, maxLevelSeenSoFar, level + 1, ans);
+    }
+
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        int maxLevelSeenSoFar = -1;
+        helper(root, maxLevelSeenSoFar, 0, ans);
+        return ans;
+    }
+};
