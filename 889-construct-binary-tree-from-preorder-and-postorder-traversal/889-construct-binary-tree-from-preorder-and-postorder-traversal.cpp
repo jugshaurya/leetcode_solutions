74c6919f950c0ce33b46fbc25999b2c941bef8f5
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-07-05 18:58:31 +0530

    Time: 23 ms (45.38%), Space: 26 MB (25.12%) - LeetHub

---
+++ 889-construct-binary-tree-from-preorder-and-postorder-traversal/889-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    map<int,int> postOrderMap;
+    TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder, int preS, int preE, int postS, int postE) {
+        
+        if(preS > preE) return NULL;
+        if(postS > postE) return NULL;
+        if(preS == preE) return new TreeNode(preorder[preS]);
+        
+        TreeNode* root = new TreeNode(preorder[preS]);
+        // find the index of next element
+        int index = postOrderMap[preorder[preS + 1]];
+        int numberOfElementsInLeft = index - postS + 1;
+        
+        root->left = constructFromPrePost(preorder, postorder, preS + 1, preS + numberOfElementsInLeft, postS, index);
+        root->right = constructFromPrePost(preorder, postorder, preS + numberOfElementsInLeft+1, preE, index + 1, postE - 1);
+        return root;
+    }
+    
+    TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
+        int n = preorder.size();
+        for(int i=0;i<n;i++) postOrderMap[postorder[i]] = i;
+        return constructFromPrePost(preorder, postorder, 0, n-1, 0, n-1);
+    }
+};
