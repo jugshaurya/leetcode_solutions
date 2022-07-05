1202161253a292c11af8270d18163fbacba192d3
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-07-05 16:12:11 +0530

    Time: 68 ms (6.76%), Space: 23.2 MB (63.96%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -8,31 +8,38 @@
  * };
  */
 
+// Method 1:
+// Can be done like LCA of BT, but it is O(n)
+// class Solution {
+// public:
+//      TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+//         if(root == NULL) return NULL;
+        
+//         if(root->val == p->val or root->val == q->val) return root;
+        
+//         TreeNode* leftAns = lowestCommonAncestor(root->left, p, q);
+//         TreeNode* rightAns = lowestCommonAncestor(root->right, p, q);
+        
+//         if(leftAns == NULL and rightAns==NULL) return NULL;
+//         if(leftAns == NULL) return rightAns;
+//         if(rightAns == NULL) return leftAns;
+        
+//         return root;
+//     }
+// };
+
+// Method 2: use BST property and do it in O(H) (logn in some cases)
 class Solution {
 public:
-//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-//         while(root!=NULL){
-//             if(root->val > p->val and root->val > q->val) root = root->left;
-//             else if(root->val < p->val and root->val < q->val  ) root=root->right;
-//             else return root;
-//         }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         
-//         return NULL;
-//     }
-    
-     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(root == NULL) return NULL;
         
-        if(root->val == p->val or root->val == q->val) return root;
-        
-        TreeNode* leftAns = lowestCommonAncestor(root->left, p, q);
-        TreeNode* rightAns = lowestCommonAncestor(root->right, p, q);
-        
-        if(leftAns == NULL and rightAns==NULL) return NULL;
-        if(leftAns == NULL) return rightAns;
-        if(rightAns == NULL) return leftAns;
-        
+        if(p->val < root->val and q->val > root->val) return root;
+        if(p->val < root->val and q->val < root->val) return lowestCommonAncestor(root->left, p, q);
+        if(p->val > root->val and q->val > root->val) return lowestCommonAncestor(root->right, p, q);
         return root;
-        
     }
+    
+  
 };
