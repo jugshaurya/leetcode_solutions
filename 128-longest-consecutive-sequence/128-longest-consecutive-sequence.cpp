f0ba18ff5b4c4a4d27a8db03d5969213ba52159d
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-07-06 19:32:05 +0530

    Time: 1166 ms (10.47%), Space: 49.8 MB (53.34%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -4,14 +4,14 @@ public:
         unordered_set<int> s;
         for(auto x: nums) s.insert(x);
         
-        int ans = 0;
+        int longestSeq = 0;
         for(auto x: nums) {
             if(s.count(x - 1)) continue;
             int tmp = x + 1;
             while(s.count(tmp) == 1) tmp++;
-            ans = max(ans, tmp - x);
+            longestSeq = max(longestSeq, tmp - x);
         }
         
-        return ans;
+        return longestSeq;
     }
 };
