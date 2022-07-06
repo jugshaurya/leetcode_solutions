d6d0d117a03a47199b42058d9e6743bb5993f9c7
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-07-06 11:56:51 +0530

    Time: 9 ms (50.48%), Space: 7.9 MB (72.90%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,11 +1,25 @@
+// Method 1
+// class Solution {
+// public:
+//     vector<int> countBits(int n) {
+//         // O(n)
+//         vector<int> dp(n+1,0);
+//         for(int i = 1; i <= n; i++){
+//             dp[i] = dp[i >> 1] + (i & 1);
+//         }
+        
+//         return dp;
+//     }
+// };
+
+// Method 2
 class Solution {
 public:
     vector<int> countBits(int n) {
-       
         // O(n)
         vector<int> dp(n+1,0);
         for(int i = 1; i <= n; i++){
-            dp[i] = dp[i >> 1] + (i & 1);
+            dp[i] = dp[i & (i - 1)] + 1;
         }
         
         return dp;
