4afa425fbd0594d4534ecc9d0b2537e1046f7b5e
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-07-06 18:17:21 +0530

    Time: 229 ms (6.76%), Space: 43.9 MB (30.55%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-      
-        int n = nums.size();
-        vector<int> prefixSum(n, 0);
         map<int, int> mp;
-      
-//         for(int i=0;i<n;i++){
-//           if(i) prefixSum[i] = prefixSum[i-1] + nums[i];
-//           else prefixSum[i] = nums[i];
-//         }
-        
-//         int ans = 0;
-//         mp[0]++;
-//         for(auto x: prefixSum){
-//           if(mp.count(x-k)) ans+= mp[x-k];
-//           mp[x]++;
-//         }
-        
+        mp[0] = 1;
         int ans = 0;
-        mp[0]++;
-        for(int i=0;i<n;i++){
-          if(i) prefixSum[i] = prefixSum[i-1] + nums[i];
-          else prefixSum[i] = nums[i];
-          
-          if(mp.count(prefixSum[i]-k)) ans+= mp[prefixSum[i]-k];
-          mp[prefixSum[i]]++;
+        int sum = 0;
+        for(auto x: nums) {
+            sum += x;
+            
+            if(mp.count(sum - k)){
+                ans += mp[sum - k];
+            }
+            mp[sum]++;
         }
         
         return ans;
