fd4eebfe94a2e8a7978e45912fb8c0b1c43b5d3d
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-07-07 19:06:03 +0530

    Time: 14 ms (81.53%), Space: 22.1 MB (5.13%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -11,14 +11,16 @@
  */
 class Solution {
 public:
-    
+    // pair -> {isBalanced, height}
     pair<bool, int> isBalancedHelper(TreeNode* root) {
         if(root == NULL) return {true, 0}; 
         
         auto leftAns = isBalancedHelper(root->left);
         auto rightAns = isBalancedHelper(root->right);
         
-        return { leftAns.first and rightAns.first and abs(leftAns.second-  rightAns.second) <= 1 , 1 + max(leftAns.second, rightAns.second)};
+        int isBalanced = leftAns.first and rightAns.first and abs(leftAns.second - rightAns.second) <= 1;
+        int height = 1 + max(leftAns.second, rightAns.second); 
+        return {isBalanced, height};
     }
     
     bool isBalanced(TreeNode* root) {
