b7ceae260d306687124717cb3778b54b9054199c
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-07-09 00:48:40 +0530

    Time: 23 ms (78.45%), Space: 19.6 MB (58.23%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -5,11 +5,10 @@ public:
         int count = 0;
         int pos = -1;
         for(auto x: nums){
-            if(count == 0) {pos = x; count+=1;}
-            else {
-                if(x == pos) count++;
-                else count--;
+            if(count == 0) {
+                pos = x;
             }
+            count += x == pos ? 1 :-1;
         }
         
         return pos;
