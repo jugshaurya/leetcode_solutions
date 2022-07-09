68c6c2db0eb5892ab4ccf75523e32e39a3a38a6a
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-07-09 07:11:54 +0530

    Time: 42 ms (19.15%), Space: 29.6 MB (14.87%) - LeetHub

--- 337-house-robber-iii/337-house-robber-iii.cpp
+++ 337-house-robber-iii/337-house-robber-iii.cpp
@@ -10,52 +10,52 @@
  * };
  */
 
-
-// Method2: using(Bottom Up)
+// Method1: using DP on trees(Top Down)
 class Solution {
 public:
-    // return {ifTakenMaxSumPossible, ifNotTakenMaxSumPossible}
-    pair<int, int> helper(TreeNode* root){
-        if(root == NULL) return {0, 0};
+    map<pair<TreeNode*,bool>, int> mp;
+    int helper(TreeNode* root, bool canTake){
+        if(root == NULL) return 0;
         
-        auto leftAns = helper(root->left);
-        auto rightAns = helper(root->right);
+        if(mp.count({root, canTake}) == 1){
+            return mp[{root, canTake}];
+        }
+        // if root can be taken
+        if(canTake){
+            // i will take root
+            int opt1 = root->val + helper(root->left, false) + helper(root->right, false);
+            // i will still not take the root.
+            int opt2 = helper(root->left, true) + helper(root->right, true);
+            return mp[{root, canTake}] = max(opt1, opt2);
+        }
         
-        int ifTakenRoot = root->val + leftAns.second + rightAns.second;
-        int ifNotTakenRoot = max(leftAns.first, leftAns.second) + max(rightAns.first, rightAns.second);
-        
-        return {ifTakenRoot, ifNotTakenRoot};
+        return mp[{root, canTake}] = helper(root->left, true) + helper(root->right, true);
     }
     
     int rob(TreeNode* root) {
-        auto [a,b] = helper(root);
-        return max(a, b);
+        return max(helper(root, true), helper(root, false));
     }
 };
 
-// Method1: using DP on trees(Top Down)
+
+// Method2: using(Bottom Up)
 // class Solution {
 // public:
-//     map<pair<TreeNode*,bool>, int> mp;
-//     int helper(TreeNode* root, bool canTake){
-//         if(root == NULL) return 0;
+//     // return {ifTakenMaxSumPossible, ifNotTakenMaxSumPossible}
+//     pair<int, int> helper(TreeNode* root){
+//         if(root == NULL) return {0, 0};
         
-//         if(mp.count({root, canTake}) == 1){
-//             return mp[{root, canTake}];
-//         }
-//         // if root can be taken
-//         if(canTake){
-//             // i will take root
-//             int opt1 = root->val + helper(root->left, false) + helper(root->right, false);
-//             // i will still not take the root.
-//             int opt2 = helper(root->left, true) + helper(root->right, true);
-//             return mp[{root, canTake}] = max(opt1, opt2);
-//         }
+//         auto leftAns = helper(root->left);
+//         auto rightAns = helper(root->right);
         
-//         return mp[{root, canTake}] = helper(root->left, true) + helper(root->right, true);
+//         int ifTakenRoot = root->val + leftAns.second + rightAns.second;
+//         int ifNotTakenRoot = max(leftAns.first, leftAns.second) + max(rightAns.first, rightAns.second);
+        
+//         return {ifTakenRoot, ifNotTakenRoot};
 //     }
     
 //     int rob(TreeNode* root) {
-//         return max(helper(root, true), helper(root, false));
+//         auto [a,b] = helper(root);
+//         return max(a, b);
 //     }
 // };
