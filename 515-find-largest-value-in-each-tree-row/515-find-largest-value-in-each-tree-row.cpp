9a3e70b5516a189f84b8c7d4f778e793e8ab1509
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-07-11 21:04:54 +0530

    Time: 18 ms (63.39%), Space: 22.1 MB (87.72%) - LeetHub

--- 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.cpp
+++ 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.cpp
@@ -11,37 +11,55 @@
  */
 
 // Method1: Using Level Order Traversal
-// class Solution {
-// public:
-//     vector<int> largestValues(TreeNode* root) {
-        
-//     }
-// };
-
-// Method2: Using Postorder Traversal
 class Solution {
 public:
     vector<int> largestValues(TreeNode* root) {
         if(root == NULL) return {};
-        
-        vector<int> leftAns = largestValues(root->left);
-        vector<int> rightAns = largestValues(root->right);
-        
         vector<int> ans;
-        ans.push_back(root->val);
         
-        int n = leftAns.size();
-        int m = rightAns.size();
-        
-        int i = 0;
-        while(i < min(n,m)){
-            ans.push_back(max(leftAns[i], rightAns[i]));
-            i++;
-        }        
-        
-        if(n > m) while(i < n) ans.push_back(leftAns[i++]);
-        else while(i < m) ans.push_back(rightAns[i++]);
-        
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty()){
+            int sz = q.size();
+            long maxx = -1e10;
+            while(sz--){
+                auto front = q.front(); q.pop();
+                maxx = max(maxx, (long)front->val);
+
+                if(front->left) q.push(front->left);
+                if(front->right) q.push(front->right);
+            }
+            ans.push_back(maxx);
+        }
         return ans;
     }
 };
+
+// Method2: Using Postorder Traversal
+// class Solution {
+// public:
+//     vector<int> largestValues(TreeNode* root) {
+//         if(root == NULL) return {};
+        
+//         vector<int> leftAns = largestValues(root->left);
+//         vector<int> rightAns = largestValues(root->right);
+        
+//         vector<int> ans;
+//         ans.push_back(root->val);
+        
+//         int n = leftAns.size();
+//         int m = rightAns.size();
+        
+//         int i = 0;
+//         while(i < min(n,m)){
+//             ans.push_back(max(leftAns[i], rightAns[i]));
+//             i++;
+//         }        
+        
+//         if(n > m) while(i < n) ans.push_back(leftAns[i++]);
+//         else while(i < m) ans.push_back(rightAns[i++]);
+        
+//         return ans;
+//     }
+// };
