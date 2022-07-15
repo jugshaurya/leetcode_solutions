3631d3f47f48acae8648aafb32505a5bc2692f5d
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-07-15 19:15:39 +0530

    Time: 23 ms (7.13%), Space: 14 MB (8.45%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -2,16 +2,21 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int n = nums.size();
-        pair<int,int> dp[n+1];
-        int ans = INT_MIN;
-        for(int i=0; i<n; i++){
-            if(i) {
-                dp[i].first = max({nums[i], nums[i] *dp[i-1].first, nums[i]*dp[i-1].second});
-                dp[i].second = min({nums[i], nums[i] *dp[i-1].first, nums[i]*dp[i-1].second});
-            }
-            else dp[i] = {nums[i], nums[i]};
-            ans = max({ans,dp[i].first, dp[i].second});
+
+        // first value store, maximum +ve subarray sum possible.
+        // second value store, minimum -ve subarray sum possible, 
+        // because minimum of -ve will result in maximum value 
+        // if multiplied by -ve number.
+        pair<int, int> dp[n+1];
+        dp[0] = {nums[0], nums[0]};
+        int ans = nums[0];
+        
+        for(int i = 1; i < n; i++){
+            dp[i].first = max({ nums[i], nums[i] * dp[i-1].first, nums[i] * dp[i-1].second});
+            dp[i].second = min({ nums[i], nums[i] * dp[i-1].first, nums[i] * dp[i-1].second});
+            ans = max({ans, dp[i].first, dp[i].second});
         }
+        
         return ans;
     }
 };
