f2f9aa07953738bc2e770e494b70f8b20a8084d1
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-07-16 13:08:42 +0530

    Time: 99 ms (47.41%), Space: 31.8 MB (54.82%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,53 +1,54 @@
+
 class Solution {
-public:
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+  public:
+  vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        // O(logn)
         auto it = lower_bound(arr.begin(), arr.end(), x);
-        
+        int n = arr.size();
+
         vector<int> ans;
         int i, j;
-        int n = arr.size();
-        
-        if(it == arr.end()) {
+        if (it == arr.end()) {
             // k closest are k elements from back.
             i = n - 2;
-            j = n - 1 ;
+            j = n - 1;
         }
-        
-        if(it == arr.begin()) {
+
+        if (it == arr.begin()) {
             // k closest are k elements from start.
             i = 0;
             j = 1;
-        }else{
-            if(arr[i] == x) {
+        } else {
+            if (arr[i] == x) {
                 ans.push_back(arr[i]);
                 i = it - arr.begin() - 1;
                 j = it - arr.begin() + 1;
-            }else{
+            } else {
                 i = it - arr.begin() - 1;
                 j = it - arr.begin();
-            }
+          }
         }
 
-        while(i>=0 and j < n and ans.size() < k){
-            if(abs(x-arr[i]) > abs(x - arr[j])){
+        while (i >= 0 and j < n and ans.size() < k) {
+            if (abs(x - arr[i]) > abs(x - arr[j])) {
                 ans.push_back(arr[j]);
                 j++;
-            }else{
+            } else {
                 ans.push_back(arr[i]);
                 i--;
             }
         }
-        
-        while(i>=0 and ans.size() < k){
+
+        while (i >= 0 and ans.size() < k) {
             ans.push_back(arr[i]);
             i--;
         }
-        
-        while(j<n and ans.size() < k){
+
+        while (j < n and ans.size() < k) {
             ans.push_back(arr[j]);
             j++;
         }
-        
+
         sort(ans.begin(), ans.end());
         return ans;
     }
