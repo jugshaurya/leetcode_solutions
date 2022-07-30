aa6d45d14bb84969907af0ddccdc1824bff4931e
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-07-30 22:36:53 +0530

    Time: 166 ms (10.00%), Space: 7.6 MB (70.96%) - LeetHub

---
+++ 72-edit-distance/72-edit-distance.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int memo[505][505];
+    // dp(i, j)
+    int recurse(int i, int j, string& word1, string& word2) {
+        int n = word1.size();
+        int m = word2.size();
+        
+        if(n == 0) return m;
+        if(m == 0) return n;
+        if(i == n) return m - j;
+        if(j == m) return n - i;
+        
+        if(memo[i][j] != -1) return memo[i][j];
+        
+        if(word1[i] == word2[j]) return memo[i][j] = recurse(i+1, j+1, word1, word2);
+        return memo[i][j] = 1 + min({
+            recurse(i, j+1, word1, word2),
+            recurse(i+1, j, word1, word2),
+            recurse(i+1, j+1, word1, word2)
+        });
+    }
+    
+    int minDistance(string word1, string word2) {
+        memset(memo, -1, sizeof(memo));
+        return recurse(0, 0, word1, word2);
+    }
+};
+
+  
