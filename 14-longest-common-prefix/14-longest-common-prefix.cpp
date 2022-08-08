4139c80471971e5c44aacbcea1fdf7fab911e10b
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-08-08 14:49:27 +0530

    Time: 12 ms (19.62%), Space: 10 MB (10.02%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -0,0 +1,79 @@
+// Logic 1: Keep on finding LCP of two strings
+// Logic 2: sort strings and find LCP pof first & last string
+// Logic 3: Build PrefixTree(Trie) 
+
+
+// Using Trie
+struct TrieNode{
+    vector<TrieNode*> dict;
+    string val;
+    
+    TrieNode() {
+        dict = vector<TrieNode*>(26, NULL);
+    }
+};
+
+struct Trie {
+    TrieNode* root;
+    
+    Trie(){
+        root = new TrieNode();
+    }
+    
+    void insertWord(string word){
+        TrieNode* cur = root;
+        
+        for(auto x: word){
+            int idx = x - 'a'; 
+            if(cur->dict[idx] == NULL){
+                cur->dict[idx] = new TrieNode();
+                cur = cur->dict[idx];
+            }else{
+                cur = cur->dict[idx];
+            }
+        }
+        
+        // denoting the terminal, if val is not empty.
+        cur->val = word;
+    }
+    
+    string LCP(){
+        TrieNode* cur = root;
+        string ans;
+        
+        while(cur){
+            int cnt = 0;
+            int digit = -1;
+            int i=0;
+            for(auto x: cur->dict){
+                if(x != NULL) {
+                    digit = i;     
+                    cnt++;
+                }
+                i++;
+            }
+            
+            if(cnt != 1 or cur->val!="") return ans;
+            // => cnt is 1 and cur is non terminal
+            ans += (digit + 'a');
+            cur = cur->dict[digit];
+        }
+        
+        return ans;
+    }
+};
+
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        Trie t;
+        
+        for(auto x: strs) {
+            if(x.empty()) return "";
+            t.insertWord(x);
+        }
+        
+        return t.LCP();
+    }
+};
