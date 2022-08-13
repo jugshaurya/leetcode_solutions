ecb859d296b557d8d82174727cb582457eaec9dc
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-08-13 12:43:29 +0530

    Time: 15 ms (18.15%), Space: 15.8 MB (22.05%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    
+    vector<vector<int>> ans;
+    void helper(vector<int> &subans, map<int, int> &mp){
+        if(mp.empty()) {
+            ans.push_back(subans);
+            return;
+        }
+        
+        auto [key, val] = *mp.begin();
+        
+        // frequency is one
+        if(val == 1) {
+            mp.erase(key);
+            helper(subans, mp);
+            
+            subans.push_back(key);
+            helper(subans, mp);
+            subans.pop_back();
+            
+            mp[key] = val; 
+        }else {
+            mp[key] -= 1;
+            subans.push_back(key);
+            helper(subans, mp);
+            subans.pop_back();
+            mp[key] += 1;
+            
+            mp.erase(key);
+            helper(subans, mp);
+            mp[key] = val;
+        }
+        
+        return;
+    }
+    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+                
+        map<int, int> mp;
+        for(auto x: nums) mp[x]++;
+        
+        vector<int> subans;
+        helper(subans, mp);
+        
+        return ans;
+    }
+};
