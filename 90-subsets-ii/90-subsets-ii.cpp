fbfa989b2c910fb2bb551d9ddb29b7e16884a715
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-08-13 12:54:29 +0530

    Time: 3 ms (82.60%), Space: 7.6 MB (60.39%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,48 +1,79 @@
-// Method: without storing all generated substes in a set. 
+// // Common: without storing all generated substes in a set. 
 
+// // Method 1: using map at each rec call O(2^n)
+// class Solution {
+// public:
+//     vector<vector<int>> ans;
+//     void helper(vector<int> &subans, map<int, int> &mp){
+//         if(mp.empty()) {
+//             ans.push_back(subans);
+//             return;
+//         }
+        
+//         auto [key, val] = *mp.begin();
+        
+//         // frequency is one
+//         if(val == 1) {
+//             mp.erase(key);
+//             helper(subans, mp);
+            
+//             subans.push_back(key);
+//             helper(subans, mp);
+//             subans.pop_back();
+            
+//             mp[key] = val; 
+//         }else {
+//             mp[key] -= 1;
+//             subans.push_back(key);
+//             helper(subans, mp);
+//             subans.pop_back();
+            
+//             mp.erase(key);
+//             helper(subans, mp);
+//             mp[key] = val;
+//         }
+        
+//         return;
+//     }
+    
+//     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+                
+//         map<int, int> mp;
+//         for(auto x: nums) mp[x]++;
+        
+//         vector<int> subans;
+//         helper(subans, mp);
+        
+//         return ans;
+//     }
+// };
+
+
+// Method 2: using i variable on sorted array O(n * 2^n)
 class Solution {
 public:
     vector<vector<int>> ans;
-    void helper(vector<int> &subans, map<int, int> &mp){
-        if(mp.empty()) {
+    void helper(int i, vector<int> &nums, vector<int> &subans){
+        int n = nums.size();
+        if(i == n) {
             ans.push_back(subans);
             return;
         }
         
-        auto [key, val] = *mp.begin();
+        // taking ith value
+        subans.push_back(nums[i]);
+        helper(i+1, nums,subans);
+        subans.pop_back();
         
-        // frequency is one
-        if(val == 1) {
-            mp.erase(key);
-            helper(subans, mp);
-            
-            subans.push_back(key);
-            helper(subans, mp);
-            subans.pop_back();
-            
-            mp[key] = val; 
-        }else {
-            mp[key] -= 1;
-            subans.push_back(key);
-            helper(subans, mp);
-            subans.pop_back();
-            
-            mp.erase(key);
-            helper(subans, mp);
-            mp[key] = val;
-        }
-        
-        return;
+        // leaving all a[i] values present continuously as we sorted nums already
+        while(i + 1 < n and nums[i+1] == nums[i]) i++;
+        helper(i + 1, nums, subans);
     }
     
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-                
-        map<int, int> mp;
-        for(auto x: nums) mp[x]++;
-        
+        sort(nums.begin(), nums.end());
         vector<int> subans;
-        helper(subans, mp);
-        
+        helper(0, nums, subans);
         return ans;
     }
 };
