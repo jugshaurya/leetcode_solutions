94975865ad0d12fa0ea390c01763f0f6457eb932
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-09-04 11:34:09 +0530

    Time: 82 ms (12.91%), Space: 15.4 MB (29.30%) - LeetHub

---
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int n, m, l;
+    string a, b, c;
+    
+    int dp[101][101][201];
+    bool rec(int i, int j, int k) {
+        if(i + j == l and k == l) return true;
+        
+        if(dp[i][j][k] != -1) return dp[i][j][k];
+        int ans = false;
+        if(j < m and c[k] == b[j]) ans |= rec(i, j + 1, k + 1);
+        if(i < n and c[k] == a[i]) ans |= rec(i + 1, j, k + 1);
+        return dp[i][j][k] = ans;
+    }
+    
+    bool isInterleave(string s1, string s2, string s3) {
+        a = s1;
+        b = s2;
+        c = s3;
+        n = a.size();
+        m = b.size();
+        l = c.size();
+        
+        if(n + m != l) return false;
+        memset(dp, -1, sizeof(dp));
+        return rec(0, 0, 0);
+    }
+};
