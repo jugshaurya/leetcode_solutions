76fc39a674fefeb4a67be7e7804fc7c65054c455
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-09-04 11:37:24 +0530

    Time: 2 ms (84.78%), Space: 6.3 MB (74.65%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -3,15 +3,16 @@ public:
     int n, m, l;
     string a, b, c;
     
-    int dp[101][101][201];
+    int dp[101][101];
     bool rec(int i, int j, int k) {
+        
         if(i + j == l and k == l) return true;
         
-        if(dp[i][j][k] != -1) return dp[i][j][k];
+        if(dp[i][j] != -1) return dp[i][j];
         int ans = false;
-        if(c[k] == b[j]) ans |= rec(i, j + 1, k + 1);
-        if(c[k] == a[i]) ans |= rec(i + 1, j, k + 1);
-        return dp[i][j][k] = ans;
+        if(j < m and c[k] == b[j]) ans |= rec(i, j + 1, k + 1);
+        if(i < n and c[k] == a[i]) ans |= rec(i + 1, j, k + 1);
+        return dp[i][j] = ans;
     }
     
     bool isInterleave(string s1, string s2, string s3) {
@@ -27,3 +28,34 @@ public:
         return rec(0, 0, 0);
     }
 };
+
+// class Solution {
+// public:
+//     int n, m, l;
+//     string a, b, c;
+    
+//     int dp[101][101][201];
+//     bool rec(int i, int j, int k) {
+        
+//         if(i + j == l and k == l) return true;
+        
+//         if(dp[i][j][k] != -1) return dp[i][j][k];
+//         int ans = false;
+//         if(j < m and c[k] == b[j]) ans |= rec(i, j + 1, k + 1);
+//         if(i < n and c[k] == a[i]) ans |= rec(i + 1, j, k + 1);
+//         return dp[i][j][k] = ans;
+//     }
+    
+//     bool isInterleave(string s1, string s2, string s3) {
+//         a = s1;
+//         b = s2;
+//         c = s3;
+//         n = a.size();
+//         m = b.size();
+//         l = c.size();
+        
+//         if(n + m != l) return false;
+//         memset(dp, -1, sizeof(dp));
+//         return rec(0, 0, 0);
+//     }
+// };
