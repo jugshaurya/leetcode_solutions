ee2f5761bb14082d6b3fa2a87e2c7799ec782991
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-09-04 18:49:16 +0530

    Time: 447 ms (28.14%), Space: 15.6 MB (50.93%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,66 +1,64 @@
-class Solution {
+// Method 2: optimized using extra O(n^2) space 
+// Time = O(n^2)
+class Solution{
 public:
-    // Brute Force O(n^3)
-    // bool isPalindrome(string &s, int i, int j){
-    //     // Brute force O(n)
-    //     string t = s.substr(i, j-i+1);
-    //     string tt = t;
-    //     reverse(t.begin(), t.end());
-    //     return t == tt;                
-    // }
-    // string longestPalindrome(string s) {
-    //     int n = s.size();
-    //     int maxx = 0;
-    //     string ans;
-    //     for(int i=0;i<n;i++){
-    //         for(int j=i;j<n;j++){
-    //             if(isPalindrome(s,i,j)){
-    //                 if(j-i+1 > maxx){
-    //                     maxx = j-i+1;
-    //                     ans = s.substr(i,j-i+1);
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     return ans;
-    // }
-
-    // optimized using extra O(n^2) space => Time = O(n^2)
+    string s;
+    int dp[1001][1001]; 
+    
+    bool isPalindrome(int i, int j){
+        if(i >= j) return true;
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        if(s[i] == s[j]) return dp[i][j] = isPalindrome(i + 1, j - 1);
+        return dp[i][j] = false;
+    }
+    
     string longestPalindrome(string s) {
-        int n = s.size();
-        int maxx = 0;
-        string ans;
-        vector<vector<int>> isPal(n, vector<int>(n,0));
+        this->s = s;
+        memset(dp, -1, sizeof(dp));
         
-        // every single element is a palindrome in itself.
-        for(int i=0;i<n;i++){
-            isPal[i][i] = 1; 
-        }
-        
-        // filling rest of the stuff
-        // start is [0][i]
-        for(int i=1;i<n;i++){
-            for (int j =0;j<n;j++){
-                // element [0+j][i+j]
-                int row = 0+j;
-                int col = i+j;
-                if(j<n and col<n){
-                    isPal[row][col] = s[row] != s[col] ? 0 : (row+1==col ? 1 : isPal[row+1][col-1]);   
-                }
-            }
-        }
-        
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(isPal[i][j] == 1){
-                    if(j-i+1 > maxx){
-                        maxx = j-i+1;
-                        ans = s.substr(i,j-i+1);
+        int ans = 0;
+        string res;
+        for(int i = 0; i < s.size(); i++) {
+            for (int j = i; j < s.size(); j++) {
+                if(isPalindrome(i, j)) {
+                    if(ans < j - i + 1){
+                        ans = j - i + 1;
+                        res = s.substr(i, ans);
                     }
                 }
             }
         }
-        return ans;
+        
+        return res;
     }
-    
 };
+
+// Method 1: Brute Force O(n^3)
+// class Solution {
+// public:
+    // bool isPalindrome(string &s, int i, int j){
+    //     string t = s.substr(i, j - i + 1);
+    //     string tt = t;
+    //     reverse(t.begin(), t.end());
+    //     return t == tt;                
+    // }
+    
+    // string longestPalindrome(string s) {
+    //     int n = s.size();
+    //     int maxx = 0;
+    //     string ans;
+    //     for(int i = 0; i < n; i++){
+    //         for(int j = i; j < n; j++){
+    //             if(isPalindrome(s, i, j)){
+    //                 if(j - i + 1 > maxx){
+    //                     maxx = j - i + 1;
+    //                     ans = s.substr(i, j - i + 1);
+    //                 }
+    //             }
+    //         }
+    //     }
+    
+    //     return ans;
+    // }
+// };
