4ae1577ac1aa77f72bfcc86165c7f4a289d119db
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-09-04 19:10:31 +0530

    Time: 27 ms (79.15%), Space: 16 MB (50.09%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,38 +1,83 @@
-// Method 2: optimized using extra O(n^2) space 
+// Method 3: constant space, using Expand around center
 // Time = O(n^2)
 class Solution{
 public:
+    int n;
     string s;
-    int dp[1001][1001]; 
     
-    bool isPalindrome(int i, int j){
-        if(i >= j) return true;
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        if(s[i] == s[j]) return dp[i][j] = isPalindrome(i + 1, j - 1);
-        return dp[i][j] = false;
+    string expandAroundCenter(int i, int j){
+        while(i >= 0 and j < n and s[i] == s[j]){
+            i--;
+            j++;
+        }
+        return s.substr(i + 1, j - i - 1);
     }
     
     string longestPalindrome(string s) {
+        this -> n = s.size();
         this->s = s;
-        memset(dp, -1, sizeof(dp));
+        int ansLen = 0;
+        string ansStr;
         
-        int ans = 0;
-        string res;
-        for(int i = 0; i < s.size(); i++) {
-            for (int j = i; j < s.size(); j++) {
-                if(isPalindrome(i, j)) {
-                    if(ans < j - i + 1){
-                        ans = j - i + 1;
-                        res = s.substr(i, ans);
-                    }
-                }
+        for(int i = 0; i < n; i++){
+            string opt1 = expandAroundCenter(i, i);
+            string opt2 = expandAroundCenter(i, i + 1);
+            
+            if(opt1.size() > ansLen) {
+                ansLen = opt1.size();
+                ansStr = opt1;
+            }
+            
+            if(opt2.size() > ansLen) {
+                ansLen = opt2.size();
+                ansStr = opt2;
             }
         }
         
-        return res;
+        return ansStr;
     }
 };
+
+// Method 2: optimized using extra O(n^2) space 
+// Time = O(n^2)
+// class Solution{
+// public:
+//     string s;
+//     int dp[1001][1001]; 
+    
+//     // O(n^2)
+//     bool isPalindrome(int i, int j){
+//         if(i >= j) return true;
+//         if(dp[i][j] != -1) return dp[i][j];
+        
+//         if(s[i] == s[j]) return dp[i][j] = isPalindrome(i + 1, j - 1);
+//         return dp[i][j] = false;
+//     }
+    
+//     string longestPalindrome(string s) {
+//         this->s = s;
+
+//         // O(n^2)
+//         memset(dp, -1, sizeof(dp));
+        
+//         int ans = 0;
+//         string res;
+        
+//         // O(n^2)
+//         for(int i = 0; i < s.size(); i++) {
+//             for (int j = i; j < s.size(); j++) {
+//                 if(isPalindrome(i, j)) { // O(1)
+//                     if(ans < j - i + 1){
+//                         ans = j - i + 1;
+//                         res = s.substr(i, ans);
+//                     }
+//                 }
+//             }
+//         }
+        
+//         return res;
+//     }
+// };
 
 // Method 1: Brute Force O(n^3)
 // class Solution {
