59902f24f0293c2cf33139bc2e4ab814986c0f91
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-09-07 00:38:14 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (63.78%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,37 +1,32 @@
 class Solution {
 public:
-    int rob1(vector<int>& nums) {
-        vector<int> dp(nums.size()+1, 0);
+    vector<int> arr;
+    int rob1(int l, int r){
+        int n = r - l + 1;
+        int dp[n + 1];
+        
         dp[0] = 0;
-        dp[1] = nums[0];
-        for(int i=2; i<=nums.size();i++){
-            dp[i] = max(nums[i-1] + dp[i-2], dp[i-1]);
+        dp[1] = arr[l];
+
+        for(int i = 2; i <= n; i++) {
+            dp[i] = max(arr[l + i - 1] + dp[i - 2], dp[i - 1]);
         }
-        return dp[nums.size()];
+        return dp[n];
     }
 
     int rob(vector<int>& nums) {
+        arr = nums;
         int n = nums.size();
-        if(n==1) return nums[0];
-        if(n==2) return max(nums[0], nums[1]);
-        if(n==3) return max({nums[0], nums[1], nums[2]});
+
+        if(n == 1) return nums[0];
+        if(n == 2) return max(nums[0], nums[1]);
+        if(n == 3) return max({nums[0], nums[1], nums[2]});
         
-        // Case 1: assuming house 0 will always be in answer.
-        // implies house can't be house 1th and house (n-1)th.
-        vector<int> temp = vector<int>(nums.begin()+2, nums.end()-1);
-        int opt1 = nums[0] + rob1(temp);
+        // Case 1: assuming house 0 will always never be in answer.
+        int opt1 = rob1(1, n - 1);
         
-        // Case 2: assuming house (n-1) will always be in answer.
-        // implies house can't be house 0th and house (n-2)th.
-        temp = vector<int>(nums.begin()+1, nums.end()-2);
-        int opt2 = nums[n-1] + rob1(temp);
-        
-        // Case 3: assuming none of house 0 and (n-1)th will be in answer.
-        // implies house can't be house 0th and house 2th.
-        temp = vector<int>(nums.begin()+1, nums.end()-1);
-        int opt3 = rob1(temp);
-        
-        return max({opt1,opt2,opt3});
-        
+        // Case 2: assuming house (n-1) will never be in answer.
+        int opt2 = rob1(0, n - 2);
+        return max(opt1,opt2);
     }
 };
