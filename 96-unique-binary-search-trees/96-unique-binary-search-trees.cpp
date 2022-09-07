a85538e05dc84c4bc77dfed7d7a3befe42520a29
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-09-07 19:37:30 +0530

    Time: 4 ms (25.02%), Space: 5.9 MB (67.95%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -1,23 +1,41 @@
 // Logic: Every number can possibly become the root.
+// Using Iterative code
 class Solution {
 public:
-    int dp[20];
-    
-    int rec(int n){
-        if(n == 0) return 1;
+    int numTrees(int n) {
+        int dp[n + 1];
         
-        if(dp[n] != -1) return dp[n];
-        
-        int ans = 0;
+        dp[0] = 1;
         for(int i = 1; i <= n; i++){
-            ans += rec(i - 1) * rec(n - i); 
+            dp[i] = 0;
+            for(int j = 1; j <= i; j++){
+                dp[i] += dp[j - 1] * dp[i - j]; 
+            }
         }
         
-        return dp[n] = ans;
-    }
-        
-    int numTrees(int n) {
-        memset(dp, -1, sizeof(dp));
-        return rec(n);
+        return dp[n];
     }
 };
+
+// using TopDown DP
+// class Solution {
+// public:
+//     int dp[20];
+//     int rec(int i){
+//         if(i == 0) return 1;
+        
+//         if(dp[i] != -1) return dp[i];
+        
+//         int ans = 0;
+//         for(int j = 1; j <= i; j++){
+//             ans += rec(j - 1) * rec(i - j); 
+//         }
+        
+//         return dp[i] = ans;
+//     }
+
+//     int numTrees(int n) {
+//         memset(dp, -1, sizeof(dp));
+//         return rec(n);
+//     }
+// };
