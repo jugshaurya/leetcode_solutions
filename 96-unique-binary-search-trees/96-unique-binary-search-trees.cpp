0cb78e6480c0ca0d0f7040a7217c8f5b4db362f5
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-09-07 19:21:00 +0530

    Time: 4 ms (25.02%), Space: 5.8 MB (89.06%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -1,23 +1,23 @@
+// Logic: Every number can possibly become the root.
 class Solution {
 public:
-    
     int dp[20];
-    int helper(int n){
-        
+    
+    int rec(int n){
         if(n == 0) return 1;
-
+        
         if(dp[n] != -1) return dp[n];
-      
+        
         int ans = 0;
-        for(int i=1; i<=n; i++){
-            ans += helper(i - 1) * helper(n - i); 
+        for(int i = 1; i <= n; i++){
+            ans += rec(i - 1) * rec(n - i); 
         }
         
         return dp[n] = ans;
     }
         
     int numTrees(int n) {
-        memset(dp,-1,sizeof(dp));
-        return helper(n);
+        memset(dp, -1, sizeof(dp));
+        return rec(n);
     }
 };
