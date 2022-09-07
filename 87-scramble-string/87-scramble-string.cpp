a943ac66f3f0b1c7f3c97ff7b6f15dd5bef1e727
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-09-07 11:31:35 +0530

    Time: 26 ms (88.74%), Space: 6.2 MB (96.09%) - LeetHub

--- 87-scramble-string/87-scramble-string.cpp
+++ 87-scramble-string/87-scramble-string.cpp
@@ -1,39 +1,77 @@
-// Method 1: using 4 states
+// Method 2: using 3 states
 // O(n) transitions
-// Time complexity: O(n^5)
-// Space complexity: O(n^4)
+// Time complexity: O(n^4)
+// Space complexity: O(n^3)
 
 class Solution {
    public:
     string a, b;
-    int n, m;
-    int dp[31][31][31][31];
+    int dp[31][31][31];
     
-    bool rec(int l1, int r1, int l2, int r2) {
+    bool rec(int l1, int r1, int l2) {
+        int r2 = r1 - l1 + l2;
 
         if(l1 > r1) return true;
         if(l1 == r1) return a[l1] == b[l2];
         
-        if(dp[l1][r1][l2][r2]!=-1) return dp[l1][r1][l2][r2];
+        if(dp[l1][r1][l2] != -1) return dp[l1][r1][l2];
 
         bool ans = false;
         int len = r1 - l1 + 1;
         for (int i = 0; i < len - 1; i++) {
-            ans |= rec(l1, l1 + i, l2, l2 + i) and rec(l1 + i + 1, r1, l2 + i + 1, r2);
-            ans |= rec(l1, l1 + i, r2 - i , r2) and rec(l1 + i + 1, r1, l2, r2 - i - 1);
+            ans |= rec(l1, l1 + i, l2) and rec(l1 + i + 1, r1, l2 + i + 1);
+            ans |= rec(l1, l1 + i, r2 - i) and rec(l1 + i + 1, r1, l2);
         }
-        return dp[l1][r1][l2][r2] =ans;
+        return dp[l1][r1][l2] = ans;
     }
 
     bool isScramble(string s1, string s2) {
-        this->n = s1.size();
-        this->m = s2.size();
+        int n = s1.size();
+        int m = s2.size();
         this->a = s1;
         this->b = s2;
         
-        memset(dp,-1,sizeof(dp));
-        
         if(n != m) assert("Invalid Input");
-        return rec(0, n - 1, 0, m - 1);
+        
+        memset(dp,-1,sizeof(dp));
+        return rec(0, n - 1, 0);
     }
 };
+
+// // Method 1: using 4 states
+// // O(n) transitions
+// // Time complexity: O(n^5)
+// // Space complexity: O(n^4)
+
+// class Solution {
+//    public:
+//     string a, b;
+//     int n, m;
+//     int dp[31][31][31][31];
+    
+//     bool rec(int l1, int r1, int l2, int r2) {
+
+//         if(l1 > r1) return true;
+//         if(l1 == r1) return a[l1] == b[l2];
+        
+//         if(dp[l1][r1][l2][r2] != -1) return dp[l1][r1][l2][r2];
+
+//         bool ans = false;
+//         int len = r1 - l1 + 1;
+//         for (int i = 0; i < len - 1; i++) {
+//             ans |= rec(l1, l1 + i, l2, l2 + i) and rec(l1 + i + 1, r1, l2 + i + 1, r2);
+//             ans |= rec(l1, l1 + i, r2 - i , r2) and rec(l1 + i + 1, r1, l2, r2 - i - 1);
+//         }
+//         return dp[l1][r1][l2][r2] = ans;
+//     }
+
+//     bool isScramble(string s1, string s2) {
+//         int n = s1.size();
+//         int m = s2.size();
+//         this->a = s1;
+//         this->b = s2;
+//         if(n != m) assert("Invalid Input");
+//         memset(dp,-1,sizeof(dp));
+//         return rec(0, n - 1, 0, m - 1);
+//     }
+// };
