796f04cd3face0cb3ee63cf66e233ed5e1711671
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-09-07 19:06:40 +0530

    Time: 292 ms (73.41%), Space: 103.7 MB (69.78%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -1,31 +1,51 @@
-// Method 1: hashmap + DP 
+// Method 2: Greedy Works here
 class Solution {
   public:
-    int dp[100005];
-    int rec(int n) {
-        if(n < 2) return 1e9; 
-        if(n == 2 or n == 3) return 1;
-        
-        if (dp[n] != -1) return dp[n];
-        
-        return dp[n] = 1 + min(rec(n - 2), rec(n - 3));
-    }
-
-
     int minimumRounds(vector<int>& tasks) {
         int n = tasks.size();
 
         unordered_map<int, int> mp;
         for (auto x: tasks) mp[x]++;
 
-        memset(dp, -1, sizeof(dp));
-
         int ans = 0;
-        for (auto [task, freq] : mp) {
+        for (auto [_, freq] : mp) {
             if (freq == 1) return -1;
-            else ans += rec(freq);
+            ans += (freq / 3);
+            if(freq % 3 != 0) ans += 1;
         }
 
         return ans;
     }
 };
+
+// // Method 1: hashmap + DP 
+// class Solution {
+//   public:
+//     int dp[100005];
+//     int rec(int n) {
+//         if(n < 2) return 1e9; 
+//         if(n == 2 or n == 3) return 1;
+        
+//         if (dp[n] != -1) return dp[n];
+        
+//         return dp[n] = 1 + min(rec(n - 2), rec(n - 3));
+//     }
+
+
+//     int minimumRounds(vector<int>& tasks) {
+//         int n = tasks.size();
+
+//         unordered_map<int, int> mp;
+//         for (auto x: tasks) mp[x]++;
+
+//         memset(dp, -1, sizeof(dp));
+
+//         int ans = 0;
+//         for (auto [task, freq] : mp) {
+//             if (freq == 1) return -1;
+//             else ans += rec(freq);
+//         }
+
+//         return ans;
+//     }
+// };
