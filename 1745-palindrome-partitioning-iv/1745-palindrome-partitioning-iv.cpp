fd30153616b6ba18efdeb72f8db88b2fd01ee067
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-09-09 19:38:24 +0530

    Time: 1324 ms (16.91%), Space: 41.9 MB (36.52%) - LeetHub

---
+++ 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    
+    string s;
+    int n;
+    int dp[2001][2001];
+    int isPal[2001][2001];
+
+    bool isPalindrome(int i, int j){
+        if(j <= i) return true;
+        if(isPal[i][j] != -1) return isPal[i][j];
+        return isPal[i][j] = s[i] == s[j] and isPalindrome(i + 1, j - 1);
+    }
+    
+    int rec(int i, int k) {
+        if(i == n and k == 0) return true;
+        if(i == n and k > 0) return false;
+        if(k <= 0) return false;
+        
+        if(dp[i][k] != -1) return dp[i][k];
+        
+        for (int j = i; j < n; j++) {
+            if(isPalindrome(i, j)) {
+                if(rec(j + 1, k - 1)) return dp[i][k] = true;
+            }
+        }
+        
+        return dp[i][k] = false;
+    }
+    
+    bool checkPartitioning(string s) {
+        this->s = s;
+        this->n = s.size();
+
+        memset(dp, -1, sizeof(dp));
+        memset(isPal, -1, sizeof(isPal));
+        return rec(0, 3);    
+    }
+};
