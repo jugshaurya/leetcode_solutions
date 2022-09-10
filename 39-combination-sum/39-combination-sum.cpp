b7409b2cb745d3a4d015a985c78d65d0b42d39c3
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-09-10 19:15:03 +0530

    Time: 19 ms (43.70%), Space: 18 MB (33.63%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     vector<int> arr;
     vector<vector<int>> ans;
+    int n;
     
     void helper(int i, int target, vector<int>& tmp) {
-        int n = arr.size();
         if(i == n and target == 0) {
             ans.push_back(tmp);
             return;
@@ -13,14 +13,15 @@ public:
         if(i == n) return;
         if(target < 0) return;
         
-        helper(i+1, target, tmp);
+        helper(i + 1, target, tmp);
         tmp.push_back(arr[i]);
         helper(i, target - arr[i], tmp);
         tmp.pop_back();
     }
     
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        arr = candidates;
+        this->arr = candidates;
+        this->n = arr.size();
         vector<int> tmp;
         helper(0, target, tmp);
         return ans;
