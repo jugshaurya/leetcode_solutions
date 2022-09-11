cec3e4891253e021e1279dfa24d0e9ca46dd4b2d
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-09-11 12:39:32 +0530

    Time: 0 ms (100.00%), Space: 6 MB (93.27%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
-    vector<int> dp;
+    string s;
+    int n;
+    int dp[101];
     
-    int helper(string s) { 
-        int n = s.size();
-        // Case: 0 length
-        if(n == 0) return 1;
-        
-        // case: 1 length
-        if(n == 1 and s[0] == '0') return 0; 
-        if(n == 1) return 1; 
-        
-        // Case: atleast string is 2 length
-        if(s[0] == '0') return 0;
-        // Memoization
-        if(dp[n] != -1) return dp[n];
-        
-        int ans = helper(s.substr(1));
-        int num = (s[0]-'0')*10 + (s[1]-'0');
-        
-        if(num >= 1 and num <= 26){
-            ans += helper(s.substr(2));
+    int rec(int i){
+        if(i == n) return 1;
+        if(s[i] == '0') return dp[i] = 0;
+
+        if(dp[i] != -1) return dp[i];
+            
+        int ans = rec(i + 1);
+        if(i + 1 < n) {
+            int number = (s[i] - '0') * 10 + (s[i + 1] - '0');
+            if(number >= 1 and number <= 26) ans += rec(i + 2);
         }
         
-        return dp[n] = ans;
-        
+        return dp[i] = ans;
     }
     
     int numDecodings(string s) {
-        int n = s.size();
-        dp.assign(n + 1, -1);
-        return helper(s);
+        this->s = s;    
+        this->n = s.size();    
+
+        memset(dp, -1, sizeof(dp));
+        return rec(0);
     }
 };
