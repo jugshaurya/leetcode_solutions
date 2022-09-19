c3437767c20d2170ca49274824d3cef473e49563
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-09-19 11:43:42 +0530

    Time: 8 ms (45.25%), Space: 8.8 MB (50.78%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -4,11 +4,10 @@ public:
         int n = nums.size();
         int s = 0, e = n - 1;
 
-        while(s <= e) {
-	        if(s == e) return s;
-	        
+        while(s < e) {
             int mid = (s + e) / 2;
-	        if(nums[mid + 1] > nums[mid]) s = mid + 1 ;
+            
+	        if(nums[mid + 1] > nums[mid]) s = mid + 1;
 	        else e = mid;
 	    }
         
