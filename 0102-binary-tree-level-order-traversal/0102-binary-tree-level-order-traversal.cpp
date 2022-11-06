357320d446939b86e9d70f1ffc479a7b383f4c25
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-11-06 23:08:19 +0530

    Time: 3 ms (92.33%), Space: 12.6 MB (32.14%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -9,19 +9,22 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// Method 1: usign NULL as marker
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
         if(root == NULL) return {};
+        
+        vector<vector<int>> ans;
         queue<TreeNode*> q;
         q.push(root);
         q.push(NULL);
-            
-        vector<vector<int>> ans;
+
         vector<int> tmp;
         while(!q.empty()) {
-            TreeNode* front = q.front();q.pop();
-            if(front == NULL){
+            auto front = q.front(); q.pop();
+            if(front == NULL) {
                 if(!q.empty()) q.push(NULL);
                 ans.push_back(tmp);
                 tmp.clear();
@@ -35,3 +38,29 @@ public:
         return ans;
     }
 };
+
+// Method 2: Using while loop inside while loop
+// class Solution {
+// public:
+//     vector<vector<int>> levelOrder(TreeNode* root) {
+//         if(root == NULL) return {};
+        
+//         vector<vector<int>> ans;
+//         queue<TreeNode*> q;
+//         q.push(root);
+        
+//         while(!q.empty()) {
+//             int len = q.size();
+//             vector<int> tmp;
+//             while(len--){
+//                 auto front = q.front(); q.pop();
+//                 tmp.push_back(front->val);
+//                 if(front->left) q.push(front->left);
+//                 if(front->right) q.push(front->right);
+//             }
+//             ans.push_back(tmp);            
+//         }
+        
+//         return ans;
+//     }
+// };
