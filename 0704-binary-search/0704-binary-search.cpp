0c783950692b33cf3d8382c02acb65057d747561
Shaurya Singhal <shauryasinghal84@gmail.com>
2022-11-06 23:16:01 +0530

    Time: 95 ms (42.18%), Space: 27.6 MB (12.33%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,3 +1,4 @@
+// Method1: Using STL
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
@@ -7,3 +8,26 @@ public:
         return it - nums.begin();
     }
 };
+
+// Method2: from Scratch
+// class Solution {
+// public:
+    
+//     int search(vector<int>& arr, int target) {
+//         int lo = 0;
+//         int hi = arr.size()-1;
+//         int ans = -1;
+            
+//         while(lo<=hi){
+//             int mid = (lo+hi)/2;
+//             if(arr[mid]==target) return mid;
+//             if(arr[mid]<target){
+//                 lo = mid+1;
+//             }else{
+//                 hi = mid-1;
+//             }
+//         }
+        
+//         return ans;
+//     }
+// };
